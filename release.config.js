38cf45aebb6a84c2195b2d9908e04e352dd30bad
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-01-28 18:53:11 +0100

    ci: change release.config.js in order to push to docker

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 var publishCmd = `
 IMAGE_NAME="andreabiagini5/asw"
-docker build -t "$IMAGE_NAME:\${nextRelease.version}"
+docker build -t "$IMAGE_NAME:\${nextRelease.version}" .
 docker push --all-tags "$IMAGE_NAME"
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
