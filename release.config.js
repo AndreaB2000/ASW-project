50da2b27a013a1ea6e9656c0fd4ccd131118a77b
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-01-29 16:05:45 +0100

    ci: add some features to push to npm, also add hello world script

--- release.config.js
+++ release.config.js
@@ -3,8 +3,10 @@ IMAGE_NAME="andreabiagini5/asw"
 docker build -t "$IMAGE_NAME:\${nextRelease.version}" .
 docker push --all-tags "$IMAGE_NAME"
 `
+
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
+    "@semantic-release/npm",
     [
         "@semantic-release/exec",
         {
