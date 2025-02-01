4819150a24ead9093a62ef829d1dd4dc7718e6c7
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-02-01 18:30:01 +0100

    ci: add sonar cloud to the pipeline

--- src/server/jest.config.js
+++ src/server/jest.config.js
@@ -1,18 +1,20 @@
 module.exports = {
-    globals: {
-        "ts-jest": {
-            tsconfig: "tsconfig.json",
-        },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
     },
-    moduleFileExtensions: [
-        "ts",
-        "js",
-    ],
-    transform: {
-        "^.+\\.(ts|tsx)$": "ts-jest",
-    },
-    testMatch: [
-        "**/test/**/*.spec.ts",
-    ],
-    testEnvironment: "node",
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/test/**/*.spec.ts'],
+  collectCoverage: true, // Enable coverage collection
+  coverageDirectory: 'coverage', // Directory where coverage reports are saved
+  coverageReporters: ['text', 'lcov'], // Use lcov format for SonarCloud
+  testEnvironment: 'node', // Set test environment for server-side
+  collectCoverageFrom: [
+    'src/server/**/*.{js,ts}', // Specify the files to collect coverage from
+    '!src/server/**/*.test.{js,ts}', // Exclude test files from coverage
+  ],
 };
