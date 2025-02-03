e398d270da7ae438d582e7b9fd4ac122cb2f9288
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-02-03 10:20:04 +0100

    build: add changelog, license and readme file to build artifact

--- build.sh
+++ build.sh
@@ -24,3 +24,8 @@ cp src/server/package.json dist/server/
 cp src/server/package-lock.json dist/server/
 cp src/client/package.json dist/client/
 cp src/client/package-lock.json dist/client/
+
+# Copy the README.md, LICENSE, and CHANGELOG.md files to the dist folder
+cp README.md dist/
+cp LICENSE dist/
+cp CHANGELOG.md dist/
