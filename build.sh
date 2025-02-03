daf49a988170fdc88e3fc4792a1c149ccfbfdcdb
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-02-03 09:31:33 +0100

    refactor: create a separate build script

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Clean up the dist folder
+rm -rf dist
+
+# Build the server and client
+npm run build:server
+npm run build:client
+
+# Create the dist folder
+mkdir dist
+mkdir dist/client
+mkdir dist/server
+
+# Copy the built files to the dist folder
+mv src/client/dist/* dist/client/ 
+mv src/server/dist/* dist/server/
+
+# Copy the package.json and package-lock.json files to the dist folder
+cp package.json dist/
+cp package-lock.json dist/
+cp src/server/package.json dist/server/
+cp src/server/package-lock.json dist/server/
+cp src/client/package.json dist/client/
+cp src/client/package-lock.json dist/client/
