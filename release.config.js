43a71c93bc7f1a034ac4ae2f34743aba3dfe546e
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-02-03 17:35:18 +0100

    fix: move signing from release.config.js to task of release job

--- release.config.js
+++ release.config.js
@@ -4,43 +4,11 @@ docker build -t "$IMAGE_NAME:\${nextRelease.version}" .
 docker push --all-tags "$IMAGE_NAME"
 `;
 
-const prepareCmd = `
-#!/bin/bash
-
-tar -czf dist.tar.gz dist
-mv dist.tar.gz dist/dist.tar.gz
-if [[ -z "$GPG_PRIVATE_KEY" || -z "$GPG_PASSPHRASE" ]]; then
-  echo "Error: GPG_PRIVATE_KEY and GPG_PASSPHRASE environment variables must be set."
-  exit 1
-fi
-echo "$GPG_PRIVATE_KEY" | gpg --batch --import
-export GPG_TTY=$(tty)
-echo "$GPG_PASSPHRASE" | gpg --batch --yes --pinentry-mode loopback --passphrase-fd 0 --sign dist/dist.tar.gz
-gpg --armor --detach-sign dist/dist.tar.gz
-gpg --batch --yes --delete-secret-keys
-`;
-
 let config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-  [
-    '@semantic-release/npm',
-    {
-      pkgRoot: 'dist',
-    },
-  ],
-  [
-    '@semantic-release/exec',
-    {
-      prepareCmd: prepareCmd,
-      publishCmd: publishCmd,
-    },
-  ],
-  [
-    '@semantic-release/github',
-    {
-      assets: ['dist/dist.tar.gz', 'dist/dist.tar.gz.asc'],
-    },
-  ],
+  ['@semantic-release/npm', { pkgRoot: 'dist' }],
+  ['@semantic-release/exec', { publishCmd: publishCmd }],
+  ['@semantic-release/github', { assets: ['dist/dist.tar.gz', 'dist/dist.tar.gz.asc'] }],
   [
     '@semantic-release/git',
     {
