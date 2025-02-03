225afa3a015c5dbeff1d3d894384f7d98208a85b
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-02-03 11:05:19 +0100

    build: fix build.bat

--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ mkdir dist
 mkdir dist/client
 mkdir dist/server
 
+tree src/client/dist
+tree src/server/dist
+
 # Copy the built files to the dist folder
 mv src/client/dist/* dist/client/ 
 mv src/server/dist/* dist/server/
