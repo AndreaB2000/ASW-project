8813efe6f98351f2cd7612df25912785c0d1a61f
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-03-05 09:37:21 +0100

    feat(libs): add rate limit on production env and install cors lib (to be used)

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -1,24 +1,35 @@
-import express from "express";
-import logger from "morgan";
-import helmet from "helmet";
-import * as path from "path";
+import express from 'express';
+import logger from 'morgan';
+import helmet from 'helmet';
+import * as path from 'path';
+import rateLimit from 'express-rate-limit';
 
-import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
+import { errorHandler, errorNotFoundHandler } from './middlewares/errorHandler';
 
 // Routes
-import { index } from "./routes/index";
+import { index } from './routes/index';
 // Create Express server
 export const app = express();
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "pug");
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
-app.use(express.static(path.join(__dirname, "../public")));
-app.use("/", index);
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', index);
+
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1); //TODO: check if this is needed
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+    }),
+  );
+}
 
 app.use(errorNotFoundHandler);
 app.use(errorHandler);
