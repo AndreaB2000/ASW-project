274e837458eabe319e46ddf7324b6a53797047a4
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-03-31 22:54:56 +0200

    refactor: match and move objects scoping

--- src/server/src/repositories/match.ts
+++ src/server/src/repositories/match.ts
@@ -1,25 +1,26 @@
 import mongoose, { Schema } from 'mongoose';
+import { Match } from '../models/Match';
+import { Move } from '../models/Move';
 
-export const createMatch = async (
-  player1: string,
-  player2: string,
-  creationDate: Date,
-): Promise<string> => {
-  const match = new DBMatch({ player1, player2, creationDate });
-  return (await match.save())._id.toString();
+export const createMatch = async (match: Match): Promise<string> => {
+  const dbmatch = new DBMatch(match.player1, match.player2, match.creationDate);
+  return (await dbmatch.save())._id.toString();
 };
 
-export const findMatch = async (matchId: string): Promise<Match> => {
-  const match = await DBMatch.findById(matchId);
-  return match as Match;
+export const findMatch = async (matchId: string): Promise<Match | null> => {
+  return await DBMatch.findById(matchId);
 };
 
 const MatchSchema = new Schema<Match>({
-  id: { type: String, required: true },
   player1: { type: String, required: true },
   player2: { type: String, required: true },
   creationDate: { type: Date, required: true, default: Date.now },
-  moves: { type: [Move], required: true, default: [] },
+  moves: [
+    {
+      x: { type: Number, required: true },
+      y: { type: Number, required: true },
+    },
+  ],
 });
 
 const DBMatch = mongoose.model('Match', MatchSchema);
