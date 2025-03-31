ca805f04dab6056c426bc1347d5e32d990724d99
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-03-31 23:02:31 +0200

    refactor(test): separate match tests from move tests

--- src/server/test/models/match.spec.ts
+++ src/server/test/models/match.spec.ts
@@ -4,17 +4,6 @@ import * as moveFactory from '../../src/models/Move';
 import { Move } from '../../src/models/Move';
 import { describe, it, expect, beforeEach } from '@jest/globals';
 
-describe('Move', () => {
-  it('should have the correct coordinates inside, given at creation time', () => {
-    const X: number = 3;
-    const Y: number = 4;
-
-    const move = moveFactory.create(X, Y);
-    expect(move.x).toBe(X);
-    expect(move.y).toBe(Y);
-  });
-});
-
 describe('Match', () => {
   let match: Match;
   const PLAYER1 = 'Alice';
