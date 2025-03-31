cf7ca52c539c1294a025748914233324f904a990
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-03-31 22:12:07 +0200

    feat(wip): implement part of match features

--- src/server/src/repositories/match.ts
+++ src/server/src/repositories/match.ts
@@ -1,37 +1,25 @@
-import { connect } from 'http2';
 import mongoose, { Schema } from 'mongoose';
 
-export async function connectToDatabase() {
-  try {
-    await mongoose.connect('mongodb://172.0.0.12:27017', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    } as mongoose.ConnectOptions);
-
-    console.log('Connesso a MongoDB con Mongoose');
-  } catch (err) {
-    console.error('Errore di connessione:', err);
-    throw err;
-  }
-}
-
-interface Match {
-  player1: string;
-  player2: string;
-  creationDate: Date;
-}
-
-export async function createMatch(player1: string, player2: string, creationDate: Date) {
-  connectToDatabase();
+export const createMatch = async (
+  player1: string,
+  player2: string,
+  creationDate: Date,
+): Promise<string> => {
   const match = new DBMatch({ player1, player2, creationDate });
   return (await match.save())._id.toString();
-}
+};
+
+export const findMatch = async (matchId: string): Promise<Match> => {
+  const match = await DBMatch.findById(matchId);
+  return match as Match;
+};
 
 const MatchSchema = new Schema<Match>({
+  id: { type: String, required: true },
   player1: { type: String, required: true },
   player2: { type: String, required: true },
-  creationDate: { type: Date, required: true },
+  creationDate: { type: Date, required: true, default: Date.now },
+  moves: { type: [Move], required: true, default: [] },
 });
 
 const DBMatch = mongoose.model('Match', MatchSchema);
