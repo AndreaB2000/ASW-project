185a1a612b84da7b9248c5820c69b6f9cc108922
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-03-31 16:50:32 +0200

    feat(middleware): create validator middleware

---
+++ src/server/src/middlewares/validationHandler.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+
+/**
+ * Validation middleware.
+ * This middleware is used to validate the request body, query parameters, and URL parameters.
+ * @param req the request object
+ * @param res the response object
+ * @param next the next middleware function
+ * @returns {void}
+ */
+export const validationHandler = (req: Request, res: Response, next: NextFunction): void => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+    return;
+  }
+  next();
+};
