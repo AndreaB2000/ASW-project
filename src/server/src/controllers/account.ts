af9e6e1fba653b24c1896e92d6c6e0e1e09d80da
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-03-31 16:50:32 +0200

    feat(middleware): create validator middleware

--- src/server/src/controllers/account.ts
+++ src/server/src/controllers/account.ts
@@ -13,11 +13,6 @@ export const register = async (req: Request, res: Response): Promise<void> => {
       res.status(400).json({ message: 'Username and password are required' });
       return;
     }
-    const errors = validationResult(req); // this should be added as middleware
-    if (!errors.isEmpty()) {
-      res.status(422).json({ errors: errors.array() });
-      return;
-    }
     const result = await registerAccount(username, password);
     if (!result) {
       res.status(409).json({ message: 'Account already exists' });
