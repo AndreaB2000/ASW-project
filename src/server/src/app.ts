0d5429a81469f86f33df23437d4121a855beeb0b
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-03-31 16:50:32 +0200

    feat(middleware): create validator middleware

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import rateLimit from 'express-rate-limit';
 const cors = require('cors');
 import { errorHandler, errorNotFoundHandler } from './middlewares/errorHandler';
+import { validationHandler } from './middlewares/validationHandler';
 // Routes
 import { index } from './routes/index';
 import { account } from './routes/account';
@@ -41,6 +42,7 @@ app.use(
 
 app.use(errorNotFoundHandler);
 app.use(errorHandler);
+app.use(validationHandler);
 
 app.use(helmet());
 app.use(express.json());
