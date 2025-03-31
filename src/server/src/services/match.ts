72695ea665140322cb6d29fd7deae5427963078b
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-03-31 22:12:07 +0200

    feat(wip): implement part of match features

--- src/server/src/services/match.ts
+++ src/server/src/services/match.ts
@@ -1,4 +1,26 @@
-export function newMatch(player1: string, player2: string, creationDate: Date) {
-  const newMatchID = repository.createMatch(player1, player2, creationDate);
-  return newMatchID;
-}
+import * as repository from '../repositories/match';
+
+export const newMatch = async (
+  player1: string,
+  player2: string,
+  creationDate: Date,
+): Promise<string> => {
+  return repository.createMatch(player1, player2, creationDate);
+};
+
+export const addMove = async (
+  matchId: string,
+  movingPlayer: string,
+  newMove: Move,
+): Promise<boolean> => {
+  const match: Match = await repository.findMatch(matchId);
+
+  // Checks
+  if (true) {
+    return match.addMove(newMove);
+  }
+};
+
+export const getMatch = async (matchId: string): Promise<Match> => {
+  return repository.findMatch(matchId);
+};
