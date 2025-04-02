725ec8e8af8f6015b874e16e658dc4d6689c404f
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-02 23:59:17 +0200

    chore(wip): working on starting db

--- src/server/src/models/Account.ts
+++ src/server/src/models/Account.ts
@@ -1,5 +1,7 @@
+import bcrypt from 'bcrypt';
+
 /**
- * User representation with username and password used to access application.
+ * Value object representing an account.
  */
 export interface Account {
   /**
@@ -7,19 +9,36 @@ export interface Account {
    */
   get username(): string;
   /**
-   * Password of the user
+   * Hashed password of the account
    */
-  get password(): string;
+  get hashedPassword(): string;
+  /**
+   * Checks if the given password matches the hashed password.
+   * @param password the password to check
+   */
+  checkPassword(password: string): Promise<boolean>;
 }
 
 /**
- * Account factory. Creates a new account with the given username and password.
+ * Account factory. Creates a new account with the given username and password. The password will be hashed.
  * @param username the username of the user
  * @param password the password of the user
  * @returns {Account} the account object
  */
-export const create = (username: string, password: string): Account =>
-  new AccountImpl(username, password);
+export const createWithHashing = async (username: string, password: string): Promise<Account> => {
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return new AccountImpl(username, hashedPassword);
+};
+
+/**
+ * Account factory. Creates a new account with the given username and hashed password.
+ * @param username the username of the user
+ * @param hashedPassword the hashed password of the user
+ * @returns {Account} the account object
+ */
+export const create = async (username: string, hashedPassword: string): Promise<Account> =>
+  new AccountImpl(username, hashedPassword);
 
 class AccountImpl implements Account {
   private name: string;
@@ -28,12 +47,16 @@ class AccountImpl implements Account {
   get username(): string {
     return this.name;
   }
-  get password(): string {
+  get hashedPassword(): string {
     return this.psw;
   }
 
   constructor(username: string, password: string) {
     this.name = username;
     this.psw = password;
+  }
+
+  checkPassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.psw);
   }
 }
