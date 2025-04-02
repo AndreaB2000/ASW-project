85fdd072e55aa91edfc56e9af3e152499b573f5c
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-02 23:59:17 +0200

    chore(wip): working on starting db

--- src/server/src/middlewares/validationHandler.ts
+++ src/server/src/middlewares/validationHandler.ts
@@ -11,9 +11,7 @@ import { validationResult } from 'express-validator';
  */
 export const validationHandler = (req: Request, res: Response, next: NextFunction): void => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(422).json({ errors: errors.array() });
-    return;
-  }
-  next();
+  if (errors.isEmpty()) return next();
+  console.log('Validation errors:', errors.array());
+  res.status(422).json({ errors: errors.array() });
 };
