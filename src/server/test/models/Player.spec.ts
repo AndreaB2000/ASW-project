1fce057265a8e3e42f05ffbb51f5fcc0835a4ac9
Oldranda1414 <leonardo.randacio@gmail.com>
2025-04-04 10:02:59 +0200

    feat: add rating property to player model

--- src/server/test/models/Player.spec.ts
+++ src/server/test/models/Player.spec.ts
@@ -1,12 +1,17 @@
 import { Player, PlayerFactory } from '../../src/models/Player';
+import { Rating, RatingFactory } from '../../src/models/Rating';
 import { describe, it, expect } from '@jest/globals';
 
+const startingRating: number = 1500;
+const startingDeviation: number = 350;
+const startingVolatility: number = 0.06;
 const PLAYER_NAME: string = 'Alice';
+const PLAYER_RATING: Rating = RatingFactory.create(startingRating, startingDeviation, startingVolatility);
 
 describe('Player factory', () => {
 
   it('should create a new player', async () => {
-    const player: Player = PlayerFactory.create(PLAYER_NAME);
+    const player: Player = PlayerFactory.create(PLAYER_NAME, PLAYER_RATING);
 
     expect(player).not.toBeNull();
   });
@@ -16,13 +21,13 @@ describe('Player factory', () => {
 describe('Player Model', () => {
 
   it('should have correct username', async () => {
-    const player: Player = PlayerFactory.create(PLAYER_NAME);
+    const player: Player = PlayerFactory.create(PLAYER_NAME, PLAYER_RATING);
 
     expect(player.username).toBe(PLAYER_NAME);
   });
 
   it('should have a rating', async () => {
-    const player: Player = PlayerFactory.create(PLAYER_NAME);
+    const player: Player = PlayerFactory.create(PLAYER_NAME, PLAYER_RATING);
 
     expect(player.rating).not.toBeNull();
   });
