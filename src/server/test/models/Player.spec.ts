108ee03d3c76bf9643ac683fc773328a8e613fe4
Oldranda1414 <leonardo.randacio@gmail.com>
2025-04-04 09:57:37 +0200

    feat: made Player factory method static

--- src/server/test/models/Player.spec.ts
+++ src/server/test/models/Player.spec.ts
@@ -1,4 +1,4 @@
-import { Player, create } from '../../src/models/Player';
+import { Player, PlayerFactory } from '../../src/models/Player';
 import { describe, it, expect } from '@jest/globals';
 
 const PLAYER_NAME: string = 'Alice';
@@ -6,7 +6,7 @@ const PLAYER_NAME: string = 'Alice';
 describe('Player factory', () => {
 
   it('should create a new player', async () => {
-    const player: Player = create(PLAYER_NAME);
+    const player: Player = PlayerFactory.create(PLAYER_NAME);
 
     expect(player).not.toBeNull();
   });
@@ -16,9 +16,15 @@ describe('Player factory', () => {
 describe('Player Model', () => {
 
   it('should have correct username', async () => {
-    const player: Player = create(PLAYER_NAME);
+    const player: Player = PlayerFactory.create(PLAYER_NAME);
 
     expect(player.username).toBe(PLAYER_NAME);
+  });
+
+  it('should have a rating', async () => {
+    const player: Player = PlayerFactory.create(PLAYER_NAME);
+
+    expect(player.rating).not.toBeNull();
   });
 
 });
