2efcdc6eaf5b7c730a9b0f9e438cee8576a2de6b
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-04 14:42:15 +0200

    chore(docker): update how docker compose startup database

--- src/server/src/db-connection.ts
+++ src/server/src/db-connection.ts
@@ -1,13 +1,30 @@
 import mongoose from 'mongoose';
+import fs from 'fs';
+import path from 'path';
 
 /**
  * Connects to MongoDB using Mongoose.
  */
 export const connectDB = async (): Promise<void> => {
   try {
-    const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/';
-    await mongoose.connect(uri);
-    console.log('Connected to MongoDB');
+    const host = process.env.MONGO_HOST || 'mongodb';
+    const ip = process.env.MONGO_IP || '127.0.0.1';
+    const port = process.env.MONGO_PORT || '27017';
+    const dbName = process.env.MONGO_DB || 'ASW-DB';
+    const username = process.env.MONGO_USERNAME || 'user';
+    const passwordFile =
+      process.env.MONGO_PASSWORD_FILE ||
+      path.join(__dirname, '..', 'secrets', 'mongo_root_password.txt');
+    fs.readFile(passwordFile, 'utf8', async (err, password) => {
+      if (err) {
+        console.error('Error reading password file:', err);
+        return;
+      }
+      const uri = `${host}://${ip}:${port}/${dbName}`;
+      console.log(`Connecting to ${uri}`);
+      await mongoose.connect(uri);
+      console.log('Connected to MongoDB');
+    });
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
