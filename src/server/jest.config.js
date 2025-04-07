e54bee5b3bb4acba7069830283e39ab026995544
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-07 18:25:25 +0200

    chore(config): update jest config to ignore some files

--- src/server/jest.config.js
+++ src/server/jest.config.js
@@ -8,5 +8,12 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
   testEnvironment: 'node',
-  collectCoverageFrom: ['src/**/*.{js,ts}', '!src/test/**/*', '!src/server.ts', '!jest.config.ts'],
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!src/test/**/*',
+    '!src/server.ts',
+    '!jest.config.ts',
+    '!src/app.ts',
+    '!src/routes/**/*',
+  ],
 };
