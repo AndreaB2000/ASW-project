4816863422e657cdb36cc26764b6f928014491c6
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-07 17:30:33 +0200

    test(account): complete account model tests

--- src/server/test/models/Account.spec.ts
+++ src/server/test/models/Account.spec.ts
@@ -1,18 +1,36 @@
-import * as accountFactory from '../../src/models/Account';
 import { describe, it, expect } from '@jest/globals';
+import { createWithHashing, create } from '../../src/models/Account';
+import bcrypt from 'bcrypt';
+import { jest } from '@jest/globals';
 
-describe('Account Value Object', () => {
-  const name = 'testName';
-  const psw = 'testPassword';
+jest.mock('bcrypt', () => ({
+  genSalt: jest.fn(() => Promise.resolve('salt')),
+  hash: jest.fn((password, salt) => Promise.resolve(`hashed_${password}_${salt}`)),
+  compare: jest.fn((_, _1) => Promise.resolve(true)),
+}));
 
-  it('should create an account', () => {
-    const account = accountFactory.create(name, psw);
-    expect(account).toBeDefined();
+describe('Account', () => {
+  const plainPassword = 'mySecret123!';
+  const username = 'testUser';
+
+  it('should create an account with a hashed password using createWithHashing', async () => {
+    const account = await createWithHashing(username, plainPassword);
+
+    expect(account.username).toBe(username);
+    expect(account.hashedPassword).toBe(await bcrypt.hash(plainPassword, await bcrypt.genSalt()));
   });
 
-  it('should retreive username and password', () => {
-    const account = accountFactory.create(name, psw);
-    expect(account.username).toBe(name);
-    expect(account.password).toBe(psw);
+  it('should exploit bcrypt to check password with checkPassword', async () => {
+    const account = await createWithHashing(username, plainPassword);
+    await account.checkPassword("doesn't matter");
+    expect(bcrypt.compare).toBeCalledTimes(1);
+  });
+
+  it('should create an account with a provided hashed password using create', async () => {
+    const hashedPassword = await bcrypt.hash(plainPassword, await bcrypt.genSalt());
+    const account = await create(username, hashedPassword);
+
+    expect(account.username).toBe(username);
+    expect(account.hashedPassword).toBe(hashedPassword);
   });
 });
