0a580aecd6f168d49a0bd5738573638bc3def247
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-07 17:00:27 +0200

    test(middleware): correct middleware tests

--- src/server/test/middlewares/validationHandler.spec.ts
+++ src/server/test/middlewares/validationHandler.spec.ts
@@ -6,7 +6,7 @@ import { describe, beforeAll, it, expect } from '@jest/globals';
 
 describe('Validation Middleware', () => {
   let app: express.Express;
-  const mockEndpoint = (req: express.Request, res: express.Response) => {
+  const mockEndpoint = (_: express.Request, res: express.Response) => {
     res.status(200).json({ message: 'Success' });
   };
 
@@ -16,32 +16,17 @@ describe('Validation Middleware', () => {
     app.post(
       '/test',
       [
-        body('username').isString().withMessage('Username must be a string'),
-        body('password').isString().withMessage('Password must be a string'),
+        body('username').notEmpty().isString().withMessage('Username must be a string'),
+        body('password').notEmpty().isString().withMessage('Password must be a string'),
         validationHandler,
       ],
       mockEndpoint,
     );
   });
 
-  it('should return 422 if username is missing', async () => {
+  it('should return 422 if validationResult is not empty', async () => {
     const res = await request(app).post('/test').send({ password: 'test123' });
     expect(res.status).toBe(422);
-    expect(res.body.errors).toEqual(
-      expect.arrayContaining([
-        { location: 'body', msg: 'Username must be a string', path: 'username', type: 'field' },
-      ]),
-    );
-  });
-
-  it('should return 422 if password is missing', async () => {
-    const res = await request(app).post('/test').send({ username: 'testUser' });
-    expect(res.status).toBe(422);
-    expect(res.body.errors).toEqual(
-      expect.arrayContaining([
-        { location: 'body', msg: 'Password must be a string', path: 'password', type: 'field' },
-      ]),
-    );
   });
 
   it('should return 200 if valid data is provided', async () => {
