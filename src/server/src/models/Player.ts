e1959413992e095dd35ea0ac61dbd4bffc8363e9
Oldranda1414 <leonardo.randacio@gmail.com>
2025-04-07 10:51:08 +0200

    refactor: apply formatter to files

--- src/server/src/models/Player.ts
+++ src/server/src/models/Player.ts
@@ -1,45 +1,42 @@
-import { Rating } from "./Rating";
+import { Rating } from './Rating';
 
 export interface Player {
-    /**
-     * The unique identifier for the player.
-     */
-    get username(): string;
+  /**
+   * The unique identifier for the player.
+   */
+  get username(): string;
 
-    /**
-     * The player's rating.
-     */
-    get rating(): Rating;
+  /**
+   * The player's rating.
+   */
+  get rating(): Rating;
 }
 
 export class PlayerFactory {
-
-/**
- * Creates a new Player with the given username and rating.
- * @param username the username of the user
- * @param rating the rating of the user
- * @returns {Player} the player object
- */
-public static create = (username: string, rating: Rating): Player =>
+  /**
+   * Creates a new Player with the given username and rating.
+   * @param username the username of the user
+   * @param rating the rating of the user
+   * @returns {Player} the player object
+   */
+  public static create = (username: string, rating: Rating): Player =>
     new PlayerImpl(username, rating);
-
 }
 
 class PlayerImpl implements Player {
-    private _username: string;
-    private _rating: Rating;
+  private _username: string;
+  private _rating: Rating;
 
-    constructor(username: string, rating: Rating) {
-        this._username = username;
-        this._rating = rating;
-    }
+  constructor(username: string, rating: Rating) {
+    this._username = username;
+    this._rating = rating;
+  }
 
-    get username(): string {
-        return this._username;
-    }
+  get username(): string {
+    return this._username;
+  }
 
-    get rating(): Rating {
-        return this._rating;
-    }
-
+  get rating(): Rating {
+    return this._rating;
+  }
 }
--- src/server/src/models/Rating.ts
+++ src/server/src/models/Rating.ts
@@ -1,78 +1,75 @@
 export interface Rating {
-    /**
-     * Compares the current rating with another rating. 
-     * @param comparedRating true if the current rating is higher than the compared rating.
-     * @returns {boolean} true if the current rating is higher than the compared rating.
-     */
-    higherThan(comparedRating: Rating): boolean;
+  /**
+   * Compares the current rating with another rating.
+   * @param comparedRating true if the current rating is higher than the compared rating.
+   * @returns {boolean} true if the current rating is higher than the compared rating.
+   */
+  higherThan(comparedRating: Rating): boolean;
 
-    /**
-     * Exposes the rating value for comparisons.
-     */
-    valueOf(): number;
+  /**
+   * Exposes the rating value for comparisons.
+   */
+  valueOf(): number;
 
-    /**
-     * The rating value.
-     * @returns {number} the rating value
-     */
-    get value(): number;
+  /**
+   * The rating value.
+   * @returns {number} the rating value
+   */
+  get value(): number;
 
-    /**
-     * The deviation value.
-     * @returns {number} the deviation value
-     */
-    get deviation(): number;
+  /**
+   * The deviation value.
+   * @returns {number} the deviation value
+   */
+  get deviation(): number;
 
-    /**
-     * The volatility value.
-     * @returns {number} the volatility value
-     */
-    get volatility(): number;
+  /**
+   * The volatility value.
+   * @returns {number} the volatility value
+   */
+  get volatility(): number;
 }
 
 export class RatingFactory {
-
-    /**
-     * Creates a new rating with the given rating, deviation and volatility.
-     * @param rating the rating value.
-     * @param deviation the deviation value.
-     * @param volatility the volatility value.
-     * @returns the rating object.
-     */
-    public static create = (rating: number, deviation: number, volatility: number): Rating =>
+  /**
+   * Creates a new rating with the given rating, deviation and volatility.
+   * @param rating the rating value.
+   * @param deviation the deviation value.
+   * @param volatility the volatility value.
+   * @returns the rating object.
+   */
+  public static create = (rating: number, deviation: number, volatility: number): Rating =>
     new GlickoRating(rating, deviation, volatility);
-
 }
 
-
 class GlickoRating implements Rating {
-    private _value: number;
-    private _deviation: number;
-    private _volatility: number;
+  private _value: number;
+  private _deviation: number;
+  private _volatility: number;
 
-    constructor(value: number, deviation: number, volatility: number) {
-        this._value = value;
-        this._deviation = deviation;
-        this._volatility = volatility;
-    }
+  constructor(value: number, deviation: number, volatility: number) {
+    this._value = value;
+    this._deviation = deviation;
+    this._volatility = volatility;
+  }
 
-    public higherThan(comparedRating: Rating): boolean {
-        return this > comparedRating;
-    }
+  public higherThan(comparedRating: Rating): boolean {
+    return this > comparedRating;
+  }
 
-    public valueOf(): number {
-        return this._value;
-    }
+  public valueOf(): number {
+    return this._value;
+  }
 
-    get value(): number {
-        return this._value;
-    }
+  get value(): number {
+    return this._value;
+  }
 
-    get deviation(): number {
-        return this._deviation;
-    }
+  get deviation(): number {
+    return this._deviation;
+  }
 
-    get volatility(): number {
-        return this._volatility;
-    }
+  get volatility(): number {
+    return this._volatility;
+  }
 }
