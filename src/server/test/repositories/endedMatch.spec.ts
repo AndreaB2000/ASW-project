ca765f81a6f2d204d360382f19a41e7a1c2891ae
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-04-09 12:30:57 +0200

    refactor(test): use test utility function in match repository tests

--- src/server/test/repositories/endedMatch.spec.ts
+++ src/server/test/repositories/endedMatch.spec.ts
@@ -9,11 +9,14 @@ import {
 } from '../../src/repositories/endedMatch';
 import * as matchFactory from '../../src/models/Match';
 import { Match } from '../../src/models/Match';
+import { checkCalled, checkCalledWith } from '../test_utils/check-called';
 
 describe('Match Repository', () => {
   const PLAYER1 = 'Alice';
   const PLAYER2 = 'Bob';
   const PLAYER3 = 'Carl';
+  const OTHER_PLAYER = 'otherplayer';
+  const OTHER_ID = 'otherid';
   const NOW = new Date();
   //const TEST_ID = new mongoose.Types.ObjectId().toString();
   const TEST_ID = 'testid';
@@ -26,112 +29,128 @@ describe('Match Repository', () => {
 
   describe('createMatch', () => {
     it('should save a match in the database and return its match ID', async () => {
-      const saveMock = jest.fn().mockReturnValue({ ...mockMatch, _id: TEST_ID });
-      jest.spyOn(DBMatch.prototype, 'save').mockImplementation(saveMock);
-
-      const newMatchId = await createMatch(mockMatch as any);
-
-      expect(saveMock).toHaveBeenCalled();
+      const newMatchId = await checkCalled(
+        createMatch,
+        DBMatch.prototype,
+        'save',
+        { ...mockMatch, _id: TEST_ID },
+        [mockMatch],
+      );
       expect(newMatchId).toBe(TEST_ID);
     });
   });
 
   describe('findMatch', () => {
     it('should call the find function with the given match ID and return the corresponding match', async () => {
-      const findByIdMock = jest.fn(() =>
-        Promise.resolve(mockMatch),
-      ) as unknown as jest.MockedFunction<typeof DBMatch.findById>;
-      jest.spyOn(DBMatch, 'findById').mockImplementation(findByIdMock);
-
-      const foundMatch = await findMatch(TEST_ID);
-
-      expect(findByIdMock).toHaveBeenCalledWith(TEST_ID);
+      const foundMatch = await checkCalledWith(
+        findMatch,
+        [TEST_ID],
+        DBMatch,
+        'findById',
+        mockMatch,
+        [TEST_ID],
+      );
       expect(foundMatch).toBe(mockMatch);
     });
 
     it('should return null if a match with the given ID does not exist', async () => {
-      const findByIdMock = jest.fn(() => Promise.resolve(null)) as unknown as jest.MockedFunction<
-        typeof DBMatch.findById
-      >;
-      jest.spyOn(DBMatch, 'findById').mockImplementation(findByIdMock);
-
-      const foundMatch = await findMatch('otherid');
-
+      const foundMatch = await checkCalledWith(findMatch, [TEST_ID], DBMatch, 'findById', null, [
+        TEST_ID,
+      ]);
       expect(foundMatch).toBe(null);
     });
   });
 
   describe('findMatchesByPlayer', () => {
     it('should call the find function with the given player and return the corresponding matches list', async () => {
-      const findMock = jest.fn(() =>
-        Promise.resolve([mockMatch, mockUpdatedMatch]),
-      ) as unknown as jest.MockedFunction<typeof DBMatch.find>;
-      jest.spyOn(DBMatch, 'find').mockImplementation(findMock);
-
-      const foundMatch = await findMatchesByPlayer(PLAYER2);
-
-      expect(findMock).toHaveBeenCalledWith({
-        $or: [{ player1: PLAYER2 }, { player2: PLAYER2 }],
-      });
+      const foundMatch = await checkCalledWith(
+        findMatchesByPlayer,
+        [{ $or: [{ player1: PLAYER2 }, { player2: PLAYER2 }] }],
+        DBMatch,
+        'find',
+        [mockMatch, mockUpdatedMatch],
+        [PLAYER2],
+      );
       expect(foundMatch).toEqual([mockMatch, mockUpdatedMatch]);
     });
 
     it('should return null if there are no matches played by the given player', async () => {
-      const findMock = jest.fn(() =>
-        Promise.resolve([mockMatch, mockUpdatedMatch]),
-      ) as unknown as jest.MockedFunction<typeof DBMatch.find>;
-      jest.spyOn(DBMatch, 'find').mockImplementation(findMock);
-
-      const foundMatch = await findMatchesByPlayer('otherplayer');
-
-      expect(foundMatch).toEqual([mockMatch, mockUpdatedMatch]);
+      const foundMatch = await checkCalledWith(
+        findMatchesByPlayer,
+        [{ $or: [{ player1: OTHER_PLAYER }, { player2: OTHER_PLAYER }] }],
+        DBMatch,
+        'find',
+        [],
+        [OTHER_PLAYER],
+      );
+      expect(foundMatch).toEqual([]);
     });
   });
 
   describe('updateMatch', () => {
     it('should call the model update function with the correct parameters', async () => {
-      const findOneAndUpdateMock = jest.fn(() =>
-        Promise.resolve(),
-      ) as unknown as jest.MockedFunction<typeof DBMatch.findOneAndUpdate>;
-      jest.spyOn(DBMatch, 'findOneAndUpdate').mockImplementation(findOneAndUpdateMock);
-
-      await updateMatch(TEST_ID, mockUpdatedMatch);
-
-      expect(findOneAndUpdateMock).toHaveBeenCalledWith({ matchId: TEST_ID }, mockUpdatedMatch);
+      await checkCalledWith(
+        updateMatch,
+        [{ matchId: TEST_ID }, mockUpdatedMatch],
+        DBMatch,
+        'findOneAndUpdate',
+        [],
+        [TEST_ID, mockUpdatedMatch],
+      );
     });
   });
 
   describe('deleteMatch', () => {
     it('should call the model delete function with the correct parameters', async () => {
-      const deleteOneMock = jest.fn(() =>
+      await checkCalledWith(
+        deleteMatch,
+        [{ matchId: TEST_ID }],
+        DBMatch,
+        'deleteOne',
+        {
+          acknowledged: true,
+          deletedCount: 1,
+        },
+        [TEST_ID],
+      );
+
+      /* const deleteOneMock = jest.fn(() =>
         Promise.resolve({ acknowledged: true, deletedCount: 1 }),
       ) as unknown as jest.MockedFunction<typeof DBMatch.deleteOne>;
       jest.spyOn(DBMatch, 'deleteOne').mockImplementation(deleteOneMock);
 
-      const deleted = await deleteMatch(TEST_ID);
+      await deleteMatch(TEST_ID);
 
-      expect(deleteOneMock).toHaveBeenCalledWith({ matchId: TEST_ID });
+      expect(deleteOneMock).toHaveBeenCalledWith({ matchId: TEST_ID }); */
     });
 
     it('should return true if the given ID exists', async () => {
-      const deleteOneMock = jest.fn(() =>
-        Promise.resolve({ acknowledged: true, deletedCount: 1 }),
-      ) as unknown as jest.MockedFunction<typeof DBMatch.deleteOne>;
-      jest.spyOn(DBMatch, 'deleteOne').mockImplementation(deleteOneMock);
-
-      const deleted = await deleteMatch(TEST_ID);
-
+      const deleted = await checkCalledWith(
+        deleteMatch,
+        [{ matchId: TEST_ID }],
+        DBMatch,
+        'deleteOne',
+        {
+          acknowledged: true,
+          deletedCount: 1,
+        },
+        [TEST_ID],
+      );
       expect(deleted).toBe(true);
     });
 
     it('should return false if the given ID does not exist', async () => {
-      const deleteOneMock = jest.fn(() =>
-        Promise.resolve({ acknowledged: true, deletedCount: 0 }),
-      ) as unknown as jest.MockedFunction<typeof DBMatch.deleteOne>;
-      jest.spyOn(DBMatch, 'deleteOne').mockImplementation(deleteOneMock);
-
-      const deleted = await deleteMatch('otherid');
-
+      const deleted = await checkCalledWith(
+        deleteMatch,
+        [{ matchId: OTHER_ID }],
+        DBMatch,
+        'deleteOne',
+        {
+          acknowledged: true,
+          deletedCount: 0,
+        },
+        [OTHER_ID],
+      );
       expect(deleted).toBe(false);
     });
   });
