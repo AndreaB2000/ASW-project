398afdffdcdcf3009a72ce310fc4fc3972f90f2a
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-10 14:42:40 +0200

    chore: solve rebase conflicts in server app.ts

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -27,15 +27,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use('/account', account);
 app.get('/ping', (_, res) => {
   res.send('pong');
 });
 app.use('/match', match);
 app.use('/account', account);
-app.get('/ping', (_, res) => {
-  res.send('pong');
-});
 
 if (process.env.NODE_ENV === 'production') {
   app.set('trust proxy', 1); //TODO: check if this is needed
