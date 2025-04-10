2674bcf11e4036d5430bf1f07ce4e3d034f469e8
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-10 14:34:19 +0200

    chore: solve rebase conflicts in server app.ts

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -11,7 +11,6 @@ import { validationHandler } from './middlewares/validationHandler';
 // Routes
 import { match } from './routes/match';
 import { account } from './routes/account';
-import { connectDB } from './db-connection';
 
 // Create Express server
 export const app = express();
