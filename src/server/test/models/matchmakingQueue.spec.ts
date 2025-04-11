df7e1ab6009e17e38159469ec5bd636dcdcd665f
Oldranda1414 <leonardo.randacio@gmail.com>
2025-04-11 12:09:54 +0200

    feat: add matchmaking queue

---
+++ src/server/test/models/matchmakingQueue.spec.ts
@@ -0,0 +1,93 @@
+import { MatchmakingQueue, MatchmakingQueueFactory } from '../../src/models/MatchmakingQueue';
+import {
+  MatchmakingCandidate,
+  MatchmakingCandidateFactory,
+} from '../../src/models/MatchmakingCandidate';
+import { describe, it, expect, beforeEach } from '@jest/globals';
+import test from 'node:test';
+
+const candidateId1: string = 'testPlayerId1';
+const candidateId2: string = 'testPlayerId2';
+const rating1: number = 1500;
+const rating2: number = 1600;
+const waitingTime1: number = 1;
+const waitingTime2: number = 2;
+const requestTime1: Date = new Date('2023-10-01T00:00:00Z');
+const requestTime2: Date = new Date('2023-10-02T00:00:00Z');
+const candidate1: MatchmakingCandidate = MatchmakingCandidateFactory.create(
+  candidateId1,
+  rating1,
+  waitingTime1,
+  requestTime1,
+);
+const candidate2: MatchmakingCandidate = MatchmakingCandidateFactory.create(
+  candidateId2,
+  rating2,
+  waitingTime2,
+  requestTime2,
+);
+
+let testQueue: MatchmakingQueue;
+
+beforeEach(() => {
+  // Reset the state of the MatchmakingQueue before each test
+  testQueue = MatchmakingQueueFactory.create();
+});
+
+describe('Matchmaking queue factory', () => {
+  it('should create a new matchmaking queue', async () => {
+    const queue: MatchmakingQueue = MatchmakingQueueFactory.create();
+
+    expect(queue).not.toBeNull();
+  });
+
+  it('should create an empty matchmaking queue', async () => {
+    const queue: MatchmakingQueue = MatchmakingQueueFactory.create();
+
+    expect(queue.empty()).toBe(true);
+  });
+});
+
+describe('Matchmaking Queue', () => {
+  it('should be empty on creation', async () => {
+    expect(testQueue.empty()).toBe(true);
+  });
+
+  it('should not be empty after adding a candidate', async () => {
+    testQueue.add(candidate1);
+    expect(testQueue.empty()).toBe(false);
+  });
+
+  it('should have size 0 on creation', async () => {
+    expect(testQueue.size).toBe(0);
+  });
+
+  it('should have size 1 after adding a candidate', async () => {
+    testQueue.add(candidate1);
+    expect(testQueue.size).toBe(1);
+  });
+
+  it('should work as an iterator', async () => {
+    const candidates: MatchmakingCandidate[] = [candidate1, candidate2];
+    candidates.forEach(candidate => testQueue.add(candidate));
+
+    const iteratedCandidates: MatchmakingCandidate[] = Array.from(testQueue);
+
+    expect(iteratedCandidates).toEqual(candidates);
+  });
+
+  it('should have a candidate if added', async () => {
+    testQueue.add(candidate1);
+    expect(testQueue.has(candidateId1)).toBe(true);
+  });
+
+  it('should return requested candidate if present', async () => {
+    testQueue.add(candidate1);
+    expect(testQueue.get(candidateId1)).toBe(candidate1);
+  });
+
+  it('should return undefined if requested candidate is not present', async () => {
+    testQueue.add(candidate1);
+    expect(testQueue.get(candidateId2)).toBeUndefined();
+  });
+});
