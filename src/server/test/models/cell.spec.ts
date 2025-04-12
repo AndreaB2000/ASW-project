10b74c306b6492bfd6dc102f2c1a0948d67ec6d6
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-04-12 23:06:25 +0200

    test(cell): write tests for cell model

---
+++ src/server/test/models/cell.spec.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from '@jest/globals';
+import * as cellFactory from '../../src/models/Cell';
+import * as pileFactory from '../../src/models/Pile';
+import { Pile } from '../../src/models/Pile';
+
+describe('Cell', () => {
+  it('should create an empty cell with createEmpty', () => {
+    const cell = cellFactory.createEmpty();
+
+    expect(cell.pile).toBeNull();
+  });
+
+  it('should create a cell with the given parameters', () => {
+    const player = 'player';
+    const numOfGrains = 1;
+    const cell = cellFactory.create(pileFactory.create(player, numOfGrains));
+
+    expect(cell.pile).not.toBeNull();
+    expect(cell.pile.owner).toBe(player);
+    expect(cell.pile.numberOfGrains).toBe(numOfGrains);
+  });
+});
