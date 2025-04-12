1fced1988f6fa3009ebfa2671bb3aba1e8f4de59
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-04-12 23:06:25 +0200

    test(cell): write tests for cell model

--- src/server/src/models/Cell.ts
+++ src/server/src/models/Cell.ts
@@ -1,12 +1,11 @@
 import { Pile } from './Pile';
-import * as pileFactory from './Pile';
 
 export interface Cell {
   pile?: Pile;
 }
 
 class CellImpl implements Cell {
-  constructor(public pile: Pile = null) {}
+  constructor(public pile: Pile) {}
 }
 
 export const createEmpty = () => create(null);
