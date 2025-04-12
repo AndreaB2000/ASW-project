8d07b3857f3fd7ac6cdc654a06c86838d909b564
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-04-13 00:01:22 +0200

    feat(controller): implement addMove in match controller, with tests

--- src/server/src/controllers/match.ts
+++ src/server/src/controllers/match.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from 'express';
 import * as service from '../services/match';
+import * as moveFactory from '../models/Move';
 
 /**
  * PUT /match/<id>/move
+ * Adds a move to a certain match.
  */
 export const addMove = async (req: Request, res: Response): Promise<void> => {
-  const id = req.params.id;
-
-  res.status(200);
-  res.json(service.getMatch(id));
+  try {
+    const { movingPlayer, x, y } = req.body;
+    if (!movingPlayer || !x || !y) {
+      res.status(400).json({ message: 'A player and some coordinates are required' });
+      return;
+    }
+    const result = await service.addMove(req.params.id, movingPlayer, moveFactory.create(x, y));
+    if (!result) res.status(400).json({ message: "This player can't make a move" });
+    else res.status(200).json({ message: 'Move added successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error', error });
+  }
 };
