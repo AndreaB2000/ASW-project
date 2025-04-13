a68cf67ded02b0c7fafc0ffd9820e1d1e20ce8f3
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-13 16:59:50 +0200

    feat(client): create landing page (not responsive yet)

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <h1>SandPiles</h1>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/login">Login</RouterLink>
-    </nav>
-  </header>
   <RouterView />
 </template>
