98066c0eb7d0d5b8ee5be198f90f0e48e3066d78
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-04-13 14:14:53 +0200

    feat(controller): implement getMatch in match controller, with tests

--- src/server/test/controllers/match.spec.ts
+++ src/server/test/controllers/match.spec.ts
@@ -1,9 +1,10 @@
 import { jest, describe, it, expect, beforeEach } from '@jest/globals';
 import request from 'supertest';
 import express from 'express';
-import { addMove } from '../../src/controllers/match';
+import { addMove, getMatch } from '../../src/controllers/match';
 import * as matchService from '../../src/services/match';
 import * as moveFactory from '../../src/models/Move';
+import * as matchFactory from '../../src/models/Match';
 
 const app = express();
 app.use(express.json());
@@ -11,13 +12,15 @@ app.put('/match/:id/move', addMove);
 
 jest.mock('../../src/services/match', () => ({
   addMove: jest.fn(),
+  getMatch: jest.fn(),
 }));
 
+const MATCH_ID = 'testid';
+const PLAYER_IN_TURN = 'player1';
+const PLAYER_NOT_IN_TURN = 'player2';
+
 describe('PUT /match/:id/move', () => {
-  const MATCH_ID = 'testid';
   const ROUTE = `/match/${MATCH_ID}/move`;
-  const PLAYER_IN_TURN = 'player1';
-  const PLAYER_NOT_IN_TURN = 'player2';
   const IN_TURN_BODY = { movingPlayer: PLAYER_IN_TURN, x: 1, y: 2 };
   const NOT_IN_TURN_BODY = { movingPlayer: PLAYER_NOT_IN_TURN, x: 1, y: 2 };
 
@@ -84,5 +87,47 @@ describe('PUT /match/:id/move', () => {
     expect(res.status).toBe(500);
     expect(res.body.message).toBe('Internal server error');
     expect(res.body.error).toBeDefined();
+  });
+});
+
+// Add getMatch tests here
+app.get('/match/:id', getMatch);
+
+describe('getMatch', () => {
+  const ROUTE = `/match/${MATCH_ID}`;
+  const DATE = new Date();
+  const TEST_MATCH = matchFactory.create(PLAYER_IN_TURN, PLAYER_NOT_IN_TURN, DATE);
+
+  it('should return 404 if the given ID does not exist', async () => {
+    jest.mocked(matchService.getMatch).mockResolvedValue(null);
+
+    const res = await request(app).get(ROUTE);
+
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual({ message: 'This match does not exist' });
+  });
+
+  it('should return 500 if an error occurs', async () => {
+    jest.mocked(matchService.getMatch).mockRejectedValue(new Error('Generic error'));
+
+    const res = await request(app).get(ROUTE);
+
+    expect(res.status).toBe(500);
+    expect(res.body.message).toBe('Internal server error');
+    expect(res.body.error).toBeDefined();
+  });
+
+  it('should return 200 and the match if it exists', async () => {
+    jest.mocked(matchService.getMatch).mockResolvedValue(TEST_MATCH);
+
+    const res = await request(app).get(ROUTE);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: MATCH_ID,
+      ...TEST_MATCH,
+      // Object must be converted in strings to make the tests pass
+      creationDate: TEST_MATCH.creationDate.toISOString(),
+    });
   });
 });
