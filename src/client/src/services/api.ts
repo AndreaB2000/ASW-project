236e6a57065db60c1547fd0e6ba5246c477dfb73
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-14 19:04:43 +0200

    feat(wip): working on generalizing from production context in api calls (FE)

--- src/client/src/services/api.ts
+++ src/client/src/services/api.ts
@@ -1,9 +1,17 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+import path from 'path';
+
+if (import.meta.env.DEV) {
+  process.env.SERVER_IP = 'localhost';
+} else if (import.meta.env.PROD) {
+  dotenv.config({ path: path.resolve(__dirname, '..', '..', '..', '.env') });
+}
 
 /**
  * This is the API client for the application.
  * It uses axios to make HTTP requests to the backend server.
  */
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: `http://${process.env.SERVER_IP}:${process.env.SERVER_PORT}/`,
 });
