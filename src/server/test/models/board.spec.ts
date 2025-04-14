21f5524138b135dc9739e94365dfcd60ee31292e
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-04-14 10:14:19 +0200

    feat(wip): begin implementation of board moves processing

--- src/server/test/models/board.spec.ts
+++ src/server/test/models/board.spec.ts
@@ -2,6 +2,7 @@ import { describe, it, expect } from '@jest/globals';
 import * as boardFactory from '../../src/models/Board';
 import * as pileFactory from '../../src/models/Pile';
 import * as cellFactory from '../../src/models/Cell';
+import * as moveFactory from '../../src/models/Move';
 
 describe('Board', () => {
   const PLAYER1 = 'player1';
@@ -83,5 +84,189 @@ describe('Board', () => {
     board.setCell(X, Y, cellFactory.createEmpty());
 
     expect(board.getCell(X, Y).pile).toBeNull();
+  });
+
+  it('should apply a move on an empty cell', () => {
+    const board = boardFactory.createDefault(PLAYER1, PLAYER2);
+    const X = 3;
+    const Y = 3;
+    const move = moveFactory.create(X, Y);
+
+    expect(board.getCell(3, 3).pile).toBeNull();
+
+    board.applyMove(PLAYER1, move);
+
+    expect(board.getCell(X, Y).pile).not.toBeNull();
+    expect(board.getCell(X, Y).pile?.owner).toBe(PLAYER1);
+    expect(board.getCell(X, Y).pile?.numberOfGrains).toBe(1);
+  });
+
+  it('should apply a move on an existing pile', () => {
+    const board = boardFactory.createDefault(PLAYER1, PLAYER2);
+    const X = 3;
+    const Y = 3;
+    const move = moveFactory.create(X, Y);
+
+    board.applyMove(PLAYER1, move);
+    board.applyMove(PLAYER1, move);
+
+    expect(board.getCell(X, Y).pile).not.toBeNull();
+    expect(board.getCell(X, Y).pile?.owner).toBe(PLAYER1);
+    expect(board.getCell(X, Y).pile?.numberOfGrains).toBe(2);
+  });
+
+  it('should make a pile collapse', () => {
+    const board = boardFactory.createDefault(PLAYER1, PLAYER2);
+    const X = 3;
+    const Y = 3;
+    const move = moveFactory.create(X, Y);
+
+    board.applyMove(PLAYER1, move);
+    board.applyMove(PLAYER1, move);
+    board.applyMove(PLAYER1, move);
+    board.applyMove(PLAYER1, move);
+
+    expect(board.getCell(X, Y).pile).toBeNull();
+
+    [
+      [X - 1, Y],
+      [X + 1, Y],
+      [X, Y + 1],
+      [X, Y - 1],
+    ].forEach(([x, y]) => {
+      expect(board.getCell(x, y).pile).not.toBeNull();
+      expect(board.getCell(x, y).pile?.owner).toBe(PLAYER1);
+      expect(board.getCell(x, y).pile?.numberOfGrains).toBe(1);
+    });
+  });
+
+  it('should behave like expected in a specific scenario', () => {
+    const board = boardFactory.createCustom(
+      PLAYER1,
+      PLAYER2,
+      boardFactory.DEFAULT_WIDTH,
+      boardFactory.DEFAULT_HEIGHT,
+      [
+        { x: 7, y: 8, pile: pileFactory.create(PLAYER1, 1) },
+        { x: 8, y: 7, pile: pileFactory.create(PLAYER1, 2) },
+        { x: 7, y: 7, pile: pileFactory.create(PLAYER1, 3) },
+        { x: 6, y: 7, pile: pileFactory.create(PLAYER1, 3) },
+        { x: 7, y: 6, pile: pileFactory.create(PLAYER1, 3) },
+        { x: 6, y: 6, pile: pileFactory.create(PLAYER1, 3) },
+        { x: 8, y: 5, pile: pileFactory.create(PLAYER1, 1) },
+        { x: 7, y: 5, pile: pileFactory.create(PLAYER1, 1) },
+      ],
+    );
+
+    const boardAfterMove = boardFactory.createCustom(
+      PLAYER1,
+      PLAYER2,
+      boardFactory.DEFAULT_WIDTH,
+      boardFactory.DEFAULT_HEIGHT,
+      [
+        { x: 7, y: 8, pile: pileFactory.create(PLAYER1, 2) },
+        { x: 6, y: 8, pile: pileFactory.create(PLAYER1, 1) },
+        { x: 8, y: 7, pile: pileFactory.create(PLAYER1, 3) },
+        { x: 7, y: 7, pile: pileFactory.create(PLAYER1, 2) },
+        { x: 6, y: 7, pile: pileFactory.create(PLAYER1, 1) },
+        { x: 5, y: 7, pile: pileFactory.create(PLAYER1, 1) },
+        { x: 8, y: 6, pile: pileFactory.create(PLAYER1, 1) },
+        { x: 7, y: 6, pile: pileFactory.create(PLAYER1, 1) },
+        { x: 6, y: 6, pile: pileFactory.create(PLAYER1, 1) },
+        { x: 5, y: 6, pile: pileFactory.create(PLAYER1, 1) },
+        { x: 8, y: 5, pile: pileFactory.create(PLAYER1, 1) },
+        { x: 7, y: 5, pile: pileFactory.create(PLAYER1, 2) },
+        { x: 6, y: 5, pile: pileFactory.create(PLAYER1, 1) },
+      ],
+    );
+
+    board.applyMove(PLAYER1, moveFactory.create(7, 7));
+
+    expect(board.state).toStrictEqual(boardAfterMove.state);
+  });
+
+  it('should behave like expected in another specific scenario', () => {
+    /*
+
+    [{},		{},		{B,1},		{A,1},		{A,2},		{},		{},		{}],
+    [{},		{},		{},		{},		{},		{},		{},		{}],
+    [{},		{},		{},		{},		{},		{},		{},		{}],
+    [{},		{},		{},		{},		{},		{},		{},		{}],
+    [{},		{},		{B,1},		{A,1},		{A,1},		{A,1},		{},		{}],
+    [{},		{B,2},		{A,2},		{A,1},		{A,1},		{A,2},		{A,2},		{}],
+    [{B,1},		{B,2},		{},		{A,3},		{A,3},		{A,1},		{A,2},		{A,1}],
+    [{},		{B,2},		{A,2},		{A,1},		{A,1},		{A,2},		{A,2},		{}]
+
+    [{},		{},		{B,1},		{A,1},		{A,1},		{A,1},		{},		{}],
+    [{},		{},		{},		{},		{},		{},		{},		{}],
+    [{},		{},		{},		{},		{},		{},		{},		{}],
+    [{},		{},		{},		{},		{},		{},		{},		{}],
+    [{},		{},		{B,1},		{A,1},		{A,1},		{A,1},		{},		{}],
+    [{},		{B,2},		{A,2},		{A,1},		{A,1},		{A,2},		{A,2},		{}],
+    [{B,1},		{A,2},		{},		{A,3},		{A,3},		{A,1},		{A,2},		{A,1}}],
+    [{},		{B,2},		{A,2},		{A,1},		{A,1},		{A,2},		{A,2},		{}]    
+
+    */
+
+    const board = boardFactory.createCustom(PLAYER1, PLAYER2, 8, 8, [
+      { x: 6, y: 7, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 7, y: 6, pile: pileFactory.create(PLAYER1, 2) },
+      { x: 6, y: 6, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 5, y: 6, pile: pileFactory.create(PLAYER1, 2) },
+      { x: 6, y: 5, pile: pileFactory.create(PLAYER1, 3) },
+      { x: 4, y: 5, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 0, y: 4, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 7, y: 4, pile: pileFactory.create(PLAYER1, 3) },
+      { x: 6, y: 4, pile: pileFactory.create(PLAYER1, 3) },
+      { x: 5, y: 4, pile: pileFactory.create(PLAYER1, 3) },
+
+      { x: 7, y: 3, pile: pileFactory.create(PLAYER2, 3) },
+      { x: 6, y: 3, pile: pileFactory.create(PLAYER2, 3) },
+      { x: 5, y: 3, pile: pileFactory.create(PLAYER2, 3) },
+      { x: 6, y: 2, pile: pileFactory.create(PLAYER2, 3) },
+      { x: 4, y: 2, pile: pileFactory.create(PLAYER2, 1) },
+      { x: 0, y: 2, pile: pileFactory.create(PLAYER2, 1) },
+      { x: 7, y: 1, pile: pileFactory.create(PLAYER2, 2) },
+      { x: 6, y: 1, pile: pileFactory.create(PLAYER2, 1) },
+      { x: 5, y: 1, pile: pileFactory.create(PLAYER2, 2) },
+      { x: 6, y: 0, pile: pileFactory.create(PLAYER2, 1) },
+    ]);
+
+    const boardAfterMove = boardFactory.createCustom(PLAYER1, PLAYER2, 8, 8, [
+      { x: 6, y: 7, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 7, y: 6, pile: pileFactory.create(PLAYER1, 2) },
+      { x: 6, y: 6, pile: pileFactory.create(PLAYER1, 2) },
+      { x: 5, y: 6, pile: pileFactory.create(PLAYER1, 2) },
+      { x: 7, y: 5, pile: pileFactory.create(PLAYER1, 2) },
+      { x: 6, y: 5, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 5, y: 5, pile: pileFactory.create(PLAYER1, 2) },
+      { x: 4, y: 5, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 0, y: 5, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 7, y: 4, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 6, y: 4, pile: pileFactory.create(PLAYER1, 3) },
+      { x: 5, y: 4, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 4, y: 4, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 0, y: 4, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 7, y: 3, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 6, y: 3, pile: pileFactory.create(PLAYER1, 3) },
+      { x: 5, y: 3, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 4, y: 3, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 0, y: 3, pile: pileFactory.create(PLAYER1, 1) },
+      { x: 7, y: 2, pile: pileFactory.create(PLAYER1, 2) },
+      { x: 5, y: 2, pile: pileFactory.create(PLAYER1, 2) },
+      { x: 6, y: 1, pile: pileFactory.create(PLAYER1, 2) },
+
+      { x: 4, y: 2, pile: pileFactory.create(PLAYER2, 1) },
+      { x: 0, y: 2, pile: pileFactory.create(PLAYER2, 1) },
+      { x: 7, y: 1, pile: pileFactory.create(PLAYER2, 2) },
+      { x: 5, y: 1, pile: pileFactory.create(PLAYER2, 2) },
+      { x: 6, y: 0, pile: pileFactory.create(PLAYER2, 1) },
+    ]);
+
+    console.log('Expected', JSON.stringify(boardAfterMove.state));
+    board.applyMove(PLAYER1, moveFactory.create(6, 5));
+    console.log('Actual', JSON.stringify(board.state));
+
+    expect(board.state).toStrictEqual(boardAfterMove.state);
   });
 });
