66047282822921325a35757837469aad2d631cbd
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-04-15 07:53:18 +0200

    chore(controller): arrange functions order

--- src/server/src/controllers/match.ts
+++ src/server/src/controllers/match.ts
@@ -3,25 +3,6 @@ import * as service from '../services/match';
 import * as moveFactory from '../models/Move';
 
 /**
- * PUT /match/:id/move
- * Adds a move to a certain match.
- */
-export const addMove = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { movingPlayer, x, y } = req.body;
-    if (!movingPlayer || !x || !y) {
-      res.status(400).json({ message: 'A player and some coordinates are required' });
-      return;
-    }
-    const result = await service.addMove(req.params.id, movingPlayer, moveFactory.create(x, y));
-    if (!result) res.status(400).json({ message: "This player can't make a move" });
-    else res.status(200).json({ message: 'Move added successfully' });
-  } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
-  }
-};
-
-/**
  * GET /match/:id
  * Returns the match corresponding to the given ID.
  */
@@ -43,6 +24,25 @@ export const getMatchesByPlayer = async (req: Request, res: Response): Promise<v
   try {
     const matchIDs = await service.getMatchesByPlayer(req.params.player);
     res.status(200).json({ player: req.params.player, matchIDs: matchIDs });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error', error });
+  }
+};
+
+/**
+ * PUT /match/:id/move
+ * Adds a move to a certain match.
+ */
+export const addMove = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { movingPlayer, x, y } = req.body;
+    if (!movingPlayer || !x || !y) {
+      res.status(400).json({ message: 'A player and some coordinates are required' });
+      return;
+    }
+    const result = await service.addMove(req.params.id, movingPlayer, moveFactory.create(x, y));
+    if (!result) res.status(400).json({ message: "This player can't make a move" });
+    else res.status(200).json({ message: 'Move added successfully' });
   } catch (error) {
     res.status(500).json({ message: 'Internal server error', error });
   }
