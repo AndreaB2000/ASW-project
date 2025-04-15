313299a1eb9479a38b196b034903ca8b70411d46
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-04-15 07:53:59 +0200

    feat(routes): add routes to match routes file

--- src/server/src/routes/match.ts
+++ src/server/src/routes/match.ts
@@ -3,4 +3,7 @@ import * as controller from '../controllers/match';
 
 export const match = Router();
 
+match.get('/:id', controller.getMatch);
+match.get('/byplayer/:player', controller.getMatchesByPlayer);
 match.put('/:id/move', controller.addMove);
+match.delete('/:id/delete', controller.deleteMatch);
