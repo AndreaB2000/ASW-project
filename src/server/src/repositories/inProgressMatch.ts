e62f7511d6ffa798f700156f4cfc6ce6c06263e8
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-04-15 07:28:33 +0200

    docs: add typedoc

--- src/server/src/repositories/inProgressMatch.ts
+++ src/server/src/repositories/inProgressMatch.ts
@@ -2,6 +2,12 @@ import mongoose from 'mongoose';
 import { Match } from '../models/Match';
 import { DBMatch } from './endedMatch';
 
+/**
+ * Saves a given match in the database.
+ *
+ * @param match the match object to be saved.
+ * @returns the ID of the saved match.
+ */
 export const createMatch = async (match: Match): Promise<string> => {
   const dbmatch = new DBMatch({
     player1: match.player1,
@@ -12,10 +18,22 @@ export const createMatch = async (match: Match): Promise<string> => {
   return (await dbmatch.save())._id.toString();
 };
 
+/**
+ * Returns the match corresponding to the provided ID, if it exists.
+ *
+ * @param matchId the desired match ID.
+ * @returns the match corresponding to the provided ID, or null if the ID does not exist.
+ */
 export const findMatch = async (matchId: string): Promise<Match | null> => {
   return await DBMatch.findById(matchId);
 };
 
+/**
+ * Returns a list of matches played by the provided player.
+ *
+ * @param player the player by which the matches are played.
+ * @returns a list of matches played by the provided player.
+ */
 export const findMatchesByPlayer = async (player: string): Promise<string[]> => {
   const matches = await DBMatch.find({
     $or: [{ player1: player }, { player2: player }],
@@ -24,10 +42,22 @@ export const findMatchesByPlayer = async (player: string): Promise<string[]> => 
 };
 
 // Maybe it can return a boolean representing the effectiveness of the update?
+/**
+ * Updates the match corresponding to the provided ID.
+ *
+ * @param matchId the ID of the match to be updated.
+ * @param newMatch the updated match object.
+ */
 export const updateMatch = async (matchId: string, newMatch: Match): Promise<void> => {
   await DBMatch.findOneAndUpdate({ matchId }, newMatch);
 };
 
+/**
+ * Deletes the match corresponding to the provided ID.
+ *
+ * @param matchId the ID of the match to be deleted.
+ * @returns true if the match has been deleted, false otherwise.
+ */
 export const deleteMatch = async (matchId: string): Promise<boolean> => {
   return (await DBMatch.deleteOne({ matchId })).deletedCount > 0;
 };
--- src/server/src/services/match.ts
+++ src/server/src/services/match.ts
@@ -4,6 +4,14 @@ import { Move } from '../models/Move';
 import * as endedMatchRepo from '../repositories/endedMatch';
 import * as inProgressMatchRepo from '../repositories/inProgressMatch';
 
+/**
+ * Creates a new match and returns its ID.
+ *
+ * @param player1 the first player.
+ * @param player2 the second player.
+ * @param creationDate the match creation date.
+ * @returns the ID of the just created match.
+ */
 export const newMatch = async (
   player1: string,
   player2: string,
@@ -13,10 +21,24 @@ export const newMatch = async (
   return inProgressMatchRepo.createMatch(match);
 };
 
+/**
+ * Returns the match corresponding to the provided ID, searching in both
+ * "in progress" and "ended" matches repositories.
+ *
+ * @param matchId the desired match ID
+ * @returns the match corresponding to the provided ID
+ */
 export const getMatch = async (matchId: string): Promise<Match | null> => {
   return inProgressMatchRepo.findMatch(matchId) ?? endedMatchRepo.findMatch(matchId);
 };
 
+/**
+ * Returns a list of matches played by the provided player, searching in both
+ * "in progress" and "ended" matches repositories.
+ *
+ * @param player the player by which the matches are played.
+ * @returns a list of matches played by the provided player.
+ */
 export const getMatchesByPlayer = async (player: string): Promise<string[]> => {
   return [
     ...(await inProgressMatchRepo.findMatchesByPlayer(player)),
@@ -24,6 +46,14 @@ export const getMatchesByPlayer = async (player: string): Promise<string[]> => {
   ];
 };
 
+/**
+ * Adds a move to the match corresponding to the provided ID, if it is valid.
+ *
+ * @param matchId the ID of the match in which the move has to be added.
+ * @param movingPlayer the player who is making the move.
+ * @param newMove the move to be added.
+ * @returns true if the move has been successfully added, false otherwise.
+ */
 export const addMove = async (
   matchId: string,
   movingPlayer: string,
@@ -47,6 +77,13 @@ export const addMove = async (
   return ret;
 };
 
+/**
+ * Deletes the match corresponding to the provided ID, searching in both
+ * "in progress" and "ended" matches repositories.
+ *
+ * @param matchId the ID of the match to be deleted.
+ * @returns true if the match has been deleted, false otherwise.
+ */
 export const deleteMatch = async (matchId: string): Promise<boolean> => {
   if (inProgressMatchRepo.deleteMatch(matchId)) {
     return true;
