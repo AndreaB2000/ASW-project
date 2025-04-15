72a52b9ceda65d426fc099d39e4627a8b4f7fd89
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-04-15 07:28:33 +0200

    docs: add typedoc

--- src/server/src/repositories/endedMatch.ts
+++ src/server/src/repositories/endedMatch.ts
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose';
 import { Match } from '../models/Match';
 
+/**
+ * Saves a given match in the database.
+ *
+ * @param match the match object to be saved.
+ * @returns the ID of the saved match.
+ */
 export const createMatch = async (match: Match): Promise<string> => {
   const dbmatch = new DBMatch({
     player1: match.player1,
@@ -11,10 +17,22 @@ export const createMatch = async (match: Match): Promise<string> => {
   return (await dbmatch.save())._id.toString();
 };
 
+/**
+ * Returns the match corresponding to the provided ID, if it exists.
+ *
+ * @param matchId the desired match ID.
+ * @returns the match corresponding to the provided ID, or null if the ID does not exist.
+ */
 export const findMatch = async (matchId: string): Promise<Match | null> => {
   return await DBMatch.findById(matchId);
 };
 
+/**
+ * Returns a list of matches played by the provided player.
+ *
+ * @param player the player by which the matches are played.
+ * @returns a list of matches played by the provided player.
+ */
 export const findMatchesByPlayer = async (player: string): Promise<string[]> => {
   const matches = await DBMatch.find({
     $or: [{ player1: player }, { player2: player }],
@@ -23,10 +41,22 @@ export const findMatchesByPlayer = async (player: string): Promise<string[]> => 
 };
 
 // Maybe it can return a boolean representing the effectiveness of the update?
+/**
+ * Updates the match corresponding to the provided ID.
+ *
+ * @param matchId the ID of the match to be updated.
+ * @param newMatch the updated match object.
+ */
 export const updateMatch = async (matchId: string, newMatch: Match): Promise<void> => {
   await DBMatch.findOneAndUpdate({ matchId }, newMatch);
 };
 
+/**
+ * Deletes the match corresponding to the provided ID.
+ *
+ * @param matchId the ID of the match to be deleted.
+ * @returns true if the match has been deleted, false otherwise.
+ */
 export const deleteMatch = async (matchId: string): Promise<boolean> => {
   return (await DBMatch.deleteOne({ matchId })).deletedCount > 0;
 };
