833e6a4e9a125a68c878e6eafbf2fe6b97da7390
Oldranda1414 <leonardo.randacio@gmail.com>
2025-04-16 16:53:49 +0200

    feat: add evaluate opponent match function

---
+++ src/server/test/services/matchmaking/opponentSelectionLogic.spec.ts
@@ -0,0 +1,25 @@
+import { PlayerFactory } from '../../../src/models/Player';
+import { RatingFactory } from '../../../src/models/Rating';
+import { evaluateOpponentMatch } from '../../../src/services/matchmaking/opponentSelectionLogic';
+import { jest, describe, it, expect } from '@jest/globals';
+
+jest.mock('../../../src/repositories/player', () => ({
+  readPlayerByUsername: jest.fn((username: string) => {
+    if(username === 'validUsername') return PlayerFactory.create(username, RatingFactory.create(1500, 0, 0));
+    return null;
+  })
+}));
+
+describe('opponentSelectionLogic', () => {
+  describe('evaluateOpponentMatch', () => {
+    it('should return false if either player does not exist', async () => {
+      const validUsername: string = 'validUsername';
+      const nonExistingUsername: string = 'nonExistingUsername'
+
+      let result = await evaluateOpponentMatch(validUsername, new Date(), nonExistingUsername, new Date());
+      expect(result).toEqual(false);
+      result = await evaluateOpponentMatch(nonExistingUsername, new Date(), validUsername, new Date());
+      expect(result).toEqual(false);
+    });
+  });
+});
