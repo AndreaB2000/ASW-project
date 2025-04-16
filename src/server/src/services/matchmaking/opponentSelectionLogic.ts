fff0f5c02ba2f0bc295bbdce5f506bd4eec92cfe
Oldranda1414 <leonardo.randacio@gmail.com>
2025-04-16 16:53:49 +0200

    feat: add evaluate opponent match function

---
+++ src/server/src/services/matchmaking/opponentSelectionLogic.ts
@@ -0,0 +1,18 @@
+import { readPlayerByUsername } from '../../repositories/player';
+
+export const evaluateOpponentMatch = async (
+  candidate1: string,
+  requestTime1: Date,
+  candidate2: string,
+  requestTime2: Date,
+): Promise<boolean> => {
+  // retrieve candidates' ratings
+  const player1 = await readPlayerByUsername(candidate1);
+  const player2 = await readPlayerByUsername(candidate2);
+
+  if (!player1 || !player2) {
+    return false;
+  }
+
+  return true;
+};
