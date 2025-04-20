123c291e88ff6211dc050e8ea73194f1e8738314
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-20 15:06:51 +0200

    build: correct client dockerfile

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -2,7 +2,7 @@
 import { RouterView } from 'vue-router';
 import { io } from 'socket.io-client';
 
-export const socket = io('ws://localhost:3000');
+// export const socket = io('ws://localhost:3000');
 </script>
 
 <template>
