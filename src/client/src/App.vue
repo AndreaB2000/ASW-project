4760ceddaf213a4841be34a50e38ca3c344c75fc
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-20 12:15:27 +0200

    chore: solve merge conflict in package-lock

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+import { io } from 'socket.io-client';
+
+export const socket = io('ws://localhost:3000');
 </script>
 
 <template>
