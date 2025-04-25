e4931444a7bff658238b06e5e48a6257c39a1095
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-25 12:24:36 +0200

    chore: fix rebase conflict on package-lock

--- src/client/src/services/api.ts
+++ src/client/src/services/api.ts
@@ -1,17 +1,20 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-import path from 'path';
 
-if (import.meta.env.DEV) {
-  process.env.SERVER_IP = 'localhost';
-} else if (import.meta.env.PROD) {
-  dotenv.config({ path: path.resolve(__dirname, '..', '..', '..', '.env') });
+let ip = 'localhost';
+let port = 3000;
+let protocol = 'http';
+
+if (import.meta.env.PROD) {
+  ip = import.meta.env.VITE_SERVER_IP;
+  port = import.meta.env.VITE_SERVER_PORT;
+  protocol = import.meta.env.VITE_SERVER_PROTOCOL;
+} else if (!import.meta.env.DEV) {
+  throw new Error('Unknown environment: ' + import.meta.env);
 }
+console.log(`[API URL]: ${protocol}://${ip}:${port}/`);
 
 /**
  * This is the API client for the application.
  * It uses axios to make HTTP requests to the backend server.
  */
-export const api = axios.create({
-  baseURL: `http://${process.env.SERVER_IP}:${process.env.SERVER_PORT}/`,
-});
+export const api = axios.create({ baseURL: `http://${ip}:${port}/` });
