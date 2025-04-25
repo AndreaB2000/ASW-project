016dd9f23ba9cdc0f0f8d96e559bf52324ba4723
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-25 13:12:17 +0200

    chore(client): remove useless code in App.vue

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import { io } from 'socket.io-client';
-
-// export const socket = io('ws://localhost:3000');
 </script>
 
 <template>
