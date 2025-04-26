e2fe5ca70a23c1e29de97d8ef4899445ee366018
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-04-26 18:20:23 +0200

    fix(account): working on restoring previous setup

--- src/server/src/sockets/registerSocketHandlers.ts
+++ src/server/src/sockets/registerSocketHandlers.ts
@@ -6,34 +6,4 @@ import { root } from '../routes/root';
 
 export const registerSocketHandlers = (io: Server): void => {
   io.on('connection', root);
-
-  const accountNamespace = io.of('/account');
-  accountNamespace.on('connection', socket => {
-    socket.on('register', async data => {
-      const req = { body: data };
-      await check('username')
-        .notEmpty()
-        .isString()
-        .withMessage('Username must be a string')
-        .run(req);
-      await check('password')
-        .notEmpty()
-        .isString()
-        .withMessage('Password must be a string')
-        .run(req);
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        console.log('Validation errors:', errors.array());
-        socket.emit('registerResult', {
-          success: false,
-        });
-      }
-      socket.emit(
-        'registerResult',
-        await controller.register(
-          await accountModel.createWithHashing(data.username, data.password),
-        ),
-      );
-    });
-  });
 };
