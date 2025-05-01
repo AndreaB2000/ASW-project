a2a1b8dd0359fe8b84c0b5bd9c0c1bb6877e7581
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-05-01 14:18:54 +0200

    feat(graphics): use MDBootstrap for match view

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import 'mdb-vue-ui-kit/css/mdb.min.css';
 </script>
 
 <template>
   <RouterView />
 </template>
+
+<style>
+body {
+  margin: 0;
+  background-color: #333;
+  color: white;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-size: 16pt;
+}
+</style>
--- src/client/src/views/MatchView.vue
+++ src/client/src/views/MatchView.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Icon from '@/components/Icon.vue';
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-vue-ui-kit';
 import { socket } from '@/services/socket';
 import { useUserStore } from '@/stores/userStore';
 import { useMatchStore } from '@/stores/matchStore';
@@ -23,13 +25,13 @@ function getMatch(matchId: string) {
 function updateBoard() {
   for (let x = 0; x < GRID_SIZE; x++) {
     for (let y = 0; y < GRID_SIZE; y++) {
-      const button = document.getElementById(`${x}-${y}`)
+      const button = document.getElementById(`${x}-${y}`);
       if (button) {
         const cell = match.currentState[x][y];
         if (cell.pile == null) {
           button.innerHTML = '';
-          button.classList.remove('player1')
-          button.classList.remove('player2')
+          button.classList.remove('player1');
+          button.classList.remove('player2');
         } else {
           button.innerHTML = cell.pile.numberOfGrains;
           if (cell.pile.owner == match.player1) {
@@ -39,7 +41,7 @@ function updateBoard() {
             button.classList.remove('player1');
             button.classList.add('player2');
           } else {
-            console.error("Something went wrong.");
+            console.error('Something went wrong.');
           }
         }
       }
@@ -71,146 +73,64 @@ socket.emit('matchmaking');
 </script>
 
 <template>
-  <section>
-    <section class="title">
-      <img src="../assets/landingIcon.svg"></img>
-      <h1>SANDPILES</h1>
-    </section>
-    <section class="content">
-      <input type="text" v-model="user.username"/>
-      <p>{{ match.player1 }}</p>
-      <section class="board">
-        <div class="grid">
-          <div v-for="x in GRID_SIZE" :key="x" class="grid-row">
-            <button
-              v-for="y in GRID_SIZE"
-              :key="`${x-1}-${y-1}`"
-              :id="`${x-1}-${y-1}`"
-              @click="handleButtonClick(x-1, y-1)"
-              class="grid-button"
-            ></button>
-          </div>
+  <MDBContainer fluid>
+    <MDBRow center class="my-5">
+      <MDBCol md="3">
+        <Icon />
+      </MDBCol>
+      <MDBCol md="9"></MDBCol>
+    </MDBRow>
+    <MDBRow center class="my-5">
+      <input type="text" v-model="user.username" />
+    </MDBRow>
+    <MDBRow center class="my-5">
+      <MDBCol md="3">{{ match.player1 }}</MDBCol>
+      <MDBCol md="6" class="d-flex justify-content-center">
+        <div
+          class="grid d-grid"
+          :style="`grid-template-columns: repeat(${GRID_SIZE}, 1fr);`"
+          style="aspect-ratio: 1; width: fit-content"
+        >
+          <button
+            v-for="index in GRID_SIZE * GRID_SIZE"
+            :key="index"
+            :id="`${Math.floor((index - 1) / GRID_SIZE)}-${(index - 1) % GRID_SIZE}`"
+            @click="handleButtonClick(Math.floor((index - 1) / GRID_SIZE), (index - 1) % GRID_SIZE)"
+            class="grid-button"
+          ></button>
         </div>
-      </section>
-      <p>{{ match.player2 }}</p>
-    </section>
-  </section>
+      </MDBCol>
+      <MDBCol md="3">{{ match.player2 }}</MDBCol>
+    </MDBRow>
+  </MDBContainer>
 </template>
 
 <style scoped lang="scss">
-section {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
+input {
+  width: 20%;
+}
 
-  .title {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: 2vw;
-    max-height: 20vh;
+.grid-button {
+  aspect-ratio: 1;
+  width: 80px;
+  font-weight: bold;
+  background-color: #f0f0f0;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  font-size: 0.8rem;
+  margin: 2px;
 
-    h1 {
-      font-size: 5rem;
-      letter-spacing: 40%;
-    }
+  /*&:hover {
+    background-color: #7e7e7e !important;
+  } */
+}
 
-    img {
-      height: 10vh;
-    }
-  }
+.player1 {
+  background-color: green;
+}
 
-  .content {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: 2vw;
-    flex-wrap: wrap;
-
-    .board {
-      width: 80%;
-      max-width: 600px;
-
-      .grid {
-        display: flex;
-        flex-direction: column;
-        gap: 5px;
-
-        .grid-row {
-          display: flex;
-          gap: 5px;
-          justify-content: center;
-        }
-
-        .grid-button {
-          aspect-ratio: 1;
-          width: 100%;
-          max-width: 60px;
-          min-width: 60px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          font-weight: bold;
-          background-color: #f0f0f0;
-          border: none;
-          border-radius: 5px;
-          cursor: pointer;
-          transition: background-color 0.3s ease;
-          font-size: 0.8rem;
-          padding: 0;
-
-          &:hover {
-            background-color: #7e7e7e !important;
-          }
-        }
-
-        .player1 {
-          background-color: green;
-        }
-
-        .player2 {
-          background-color: blue;
-        }
-      }
-    }
-
-    .description {
-      width: 35%;
-      min-width: 300px;
-
-      .buttons {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        gap: 1vw;
-        margin-top: 7vh;
-
-        button {
-          font-weight: bold;
-          background-color: #f0f0f0;
-          border: none;
-          border-radius: 20px;
-          padding: 3vh 2vw;
-          font-size: 2rem;
-          min-width: 276px;
-          cursor: pointer;
-          transition: background-color 0.3s ease;
-
-          &:hover {
-            background-color: #7e7e7e !important;
-          }
-        }
-      }
-    }
-
-    img {
-      width: 35%;
-      min-width: 300px;
-    }
-  }
+.player2 {
+  background-color: blue;
 }
 </style>
