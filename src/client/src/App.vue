21b6d3d62c24a6f5e5d090b3945b1a1b9b837e1d
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-05-01 16:20:14 +0200

    feat(graphics): implement player in match card and custom color theme

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -6,13 +6,3 @@ import 'mdb-vue-ui-kit/css/mdb.min.css';
 <template>
   <RouterView />
 </template>
-
-<style>
-body {
-  margin: 0;
-  background-color: #333;
-  color: white;
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-  font-size: 16pt;
-}
-</style>
---
+++ src/client/src/components/PlayerInMatch.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {
+  MDBCard,
+  MDBCardImg,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBBtn,
+} from 'mdb-vue-ui-kit';
+
+import userPlaceholder from '@/assets/user-placeholder.jpg';
+
+defineProps(['username']);
+// const glicko = defineProps(['glicko']);
+// const timeLeft = defineProps(['timeLeft']);
+</script>
+
+<template>
+  <div style="width: 50%">
+    <MDBCard text="center">
+      <MDBCardTitle>{{ username }}</MDBCardTitle>
+      <MDBCardImg :src="userPlaceholder" top alt="..." />
+      <MDBCardBody>
+        <MDBCardText>
+          Some quick example text to build on the card title and make up the bulk of the card's
+          content.
+        </MDBCardText>
+        <MDBBtn tag="a" href="#!" color="primary">Button</MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
+  </div>
+</template>
