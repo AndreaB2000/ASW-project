b7831f54b5df1543903384904d7aafb4391a392d
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2025-05-03 11:02:01 +0200

    refactor(client): on MatchView

--- src/client/src/views/MatchView.vue
+++ src/client/src/views/MatchView.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import Icon from '@/components/Icon.vue';
 import Match from '@/components/Match.vue';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdb-vue-ui-kit';
+import MatchOver from '@/components/MatchOver.vue';
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-vue-ui-kit';
 import { computed } from 'vue';
-import { useUserStore } from '@/stores/userStore';
 import { useMatchStore } from '@/stores/matchStore';
 
 const isMatchOver = computed(() => match.winner != null && !match.moveInProgress);
 
-const user = useUserStore();
 const match = useMatchStore();
 </script>
 
@@ -20,13 +19,7 @@ const match = useMatchStore();
       </MDBCol>
       <MDBCol md="9"></MDBCol>
     </MDBRow>
-    <div v-if="!isMatchOver">
-      <Match />
-    </div>
-    <MDBRow center v-else>
-      <h1>Match is over</h1>
-      <p v-if="match.winner == user.username">You won!</p>
-      <p v-else>{{ match.winner }} won</p>
-    </MDBRow>
+    <Match v-if="!isMatchOver" />
+    <MatchOver v-else />
   </MDBContainer>
 </template>
