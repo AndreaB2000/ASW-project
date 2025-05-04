fa2c7433d85a7dd9b3bdb097f408e13b82bda170
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-05-04 18:39:01 +0200

    feat(server): update db connection so it uses default variables only in development

--- src/server/src/config/db-connection.ts
+++ src/server/src/config/db-connection.ts
@@ -7,36 +7,50 @@ import path from 'path';
  */
 export const connectDB = async (): Promise<void> => {
   try {
-    dotenv.config({ path: path.resolve(__dirname, '..', '..', '..', '.env') });
-    const protocol = process.env.DB_PROTOCOL;
-    const username = process.env.DB_APP_USERNAME;
-    const password = process.env.DB_APP_PASSWORD;
-    const dbName = process.env.DB_NAME || 'aswdb';
-    const port = process.env.DB_PORT || '27017';
-    const ip = process.env.DB_IP || 'localhost';
-    if (!username || !password || !dbName || !port || !ip || !protocol) {
-      let errorMsg =
-        'Missing MongoDB credentials. Please verify that the following variables are defined in the .env root file.';
-      for (const key of [
-        'DB_PROTOCOL',
-        'DB_APP_USERNAME',
-        'DB_APP_PASSWORD',
-        'DB_NAME',
-        'DB_PORT',
-        'DB_IP',
-      ]) {
-        if (!process.env[key]) {
-          errorMsg += `\n- ${key}`;
+    let protocol: string;
+    let username: string;
+    let password: string;
+    let dbName: string;
+    let port: string;
+    let ip: string;
+    if (process.env.NODE_ENV !== 'production') {
+      protocol = 'mongodb';
+      username = 'aswuser';
+      password = 'password';
+      dbName = 'aswdb';
+      port = '27017';
+      ip = '172.0.0.12';
+    } else {
+      dotenv.config({ path: path.resolve(__dirname, '..', '..', '..', '..', '.env') });
+      protocol = process.env.DB_PROTOCOL;
+      username = process.env.DB_APP_USERNAME;
+      password = process.env.DB_APP_PASSWORD;
+      dbName = process.env.DB_NAME;
+      port = process.env.DB_PORT;
+      ip = process.env.DB_IP;
+      if (!username || !password || !dbName || !port || !ip || !protocol) {
+        let errorMsg =
+          'Missing MongoDB credentials. Please verify that the following variables are defined in the .env root file.';
+        for (const key of [
+          'DB_PROTOCOL',
+          'DB_APP_USERNAME',
+          'DB_APP_PASSWORD',
+          'DB_NAME',
+          'DB_PORT',
+          'DB_IP',
+        ]) {
+          if (!process.env[key]) {
+            errorMsg += `\n- ${key}`;
+          }
         }
+        throw new Error(errorMsg);
       }
-      throw new Error(errorMsg);
     }
     const uri = `${protocol}://${username}:${password}@${ip}:${port}/${dbName}`;
     console.log('Connecting to MongoDB...');
     await mongoose.connect(uri);
     console.log('MongoDB connected successfully');
   } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1);
+    throw new DOMException('DB connection failed');
   }
 };
