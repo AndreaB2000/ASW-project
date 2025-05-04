054bbd98a9e824deeaa990e48009e50e5c0a3982
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-05-04 18:39:01 +0200

    feat(server): update db connection so it uses default variables only in development

--- src/server/test/db-connection.spec.ts
+++ src/server/test/db-connection.spec.ts
@@ -1,41 +1,44 @@
 import mongoose from 'mongoose';
 import { connectDB } from '../src/config/db-connection';
-import { jest, beforeEach, afterEach, describe, it, expect } from '@jest/globals';
-import { checkCalledWith } from './test_utils/check-called';
+import { jest, describe, beforeEach, afterEach, it, expect, beforeAll } from '@jest/globals';
 import { mockConsole } from './test_utils/mock-console';
 
-beforeEach(() => {
-  process.env.DB_PROTOCOL = 'testProtocol';
-  process.env.DB_APP_USERNAME = 'testUser';
-  process.env.DB_APP_PASSWORD = 'testPassword';
-  process.env.DB_NAME = 'testDB';
-  process.env.DB_PORT = 'testPort';
-  process.env.DB_IP = 'testIP';
-  mockConsole();
-});
+jest.mock('dotenv', () => ({ config: jest.fn() }));
 
-afterEach(() => {
-  jest.resetAllMocks();
-});
+describe('connectDB', () => {
+  let mockConnect: any;
 
-jest.mock('dotenv', () => ({
-  config: jest.fn(),
-}));
+  beforeAll(mockConsole);
 
-describe('Db Connection', () => {
+  beforeEach(() => {
+    mockConnect = jest.spyOn(mongoose, 'connect').mockResolvedValueOnce({} as any);
+    process.env.NODE_ENV = 'test';
+  });
+
   afterEach(() => {
-    jest.clearAllMocks();
+    jest.resetAllMocks();
   });
 
-  it('should call mongoose.connect with values taken from process.env', async () => {
-    const testUri = 'testProtocol://testUser:testPassword@testIP:testPort/testDB';
-    const mockConnect = jest.spyOn(mongoose, 'connect').mockResolvedValueOnce({} as any);
+  it('should connect using default values in development/test mode', async () => {
     await connectDB();
-    expect(mockConnect).toHaveBeenCalledWith(testUri);
+    expect(mockConnect).toHaveBeenCalledWith('mongodb://aswuser:password@172.0.0.12:27017/aswdb');
   });
 
-  it('should throw an error if any of the required environment variables are missing', async () => {
-    delete process.env.DB_PROTOCOL;
-    await checkCalledWith(connectDB, [1], process, 'exit', null, []);
+  it('should connect using env variables in production mode', async () => {
+    process.env.NODE_ENV = 'production';
+    process.env.DB_PROTOCOL = 'mongo';
+    process.env.DB_APP_USERNAME = 'user';
+    process.env.DB_APP_PASSWORD = 'pass';
+    process.env.DB_NAME = 'name';
+    process.env.DB_PORT = 'port';
+    process.env.DB_IP = 'ip';
+    await connectDB();
+    expect(mockConnect).toHaveBeenCalledWith('mongo://user:pass@ip:port/name');
+  });
+
+  it('should throw if required env vars are missing in production', async () => {
+    process.env.NODE_ENV = 'production';
+    delete process.env.DB_APP_PASSWORD;
+    await expect(connectDB()).rejects.toThrow('DB connection failed');
   });
 });
