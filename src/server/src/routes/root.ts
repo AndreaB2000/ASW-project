5c43c2562bc545ad47f7a480e7124fa66b6cd301
Oldranda1414 <leonardo.randacio@gmail.com>
2025-05-07 16:06:41 +0200

    fix: started intergrating matchmaking with match feature

--- src/server/src/routes/root.ts
+++ src/server/src/routes/root.ts
@@ -29,35 +29,6 @@ const match = (socket: Socket) => {
   // If this is out of the function, io is null
   const io = ioHandler.getIO();
 
-  socket.on('matchmaking', async () => {
-    console.log('User is joining match');
-
-    // Add player to the queue
-    socket.join(QUEUE_ROOM);
-
-    // Start match if two players are in queue
-    if (io.sockets.adapter.rooms.get(QUEUE_ROOM).size == 2) {
-      console.log('There are 2 players in the queue: creating match');
-
-      // Create the match getting its ID
-      console.log('Creating new match');
-      const matchId: string = await matchService.newMatch('player1', 'player2', new Date());
-
-      // Remove sockets from queue and add them to the new match room
-      // (now it affects all sockets in the queue, in the future only
-      // the two involved in the new match will be moved)
-      console.log(`Moving sockets in match room (${matchId})`);
-      (await io.in(QUEUE_ROOM).fetchSockets()).forEach(s => {
-        s.leave(QUEUE_ROOM);
-        s.join(matchId);
-      });
-
-      // Sending matchStart message
-      console.log('Sending matchStart message');
-      io.to(matchId).emit('matchStart', matchId);
-    }
-  });
-
   socket.on('getMatch', async (matchId, callback) => {
     try {
       const match = await matchService.getMatch(matchId);
