cc565612497940a50623a237a7c9404847a33fe8
Oldranda1414 <leonardo.randacio@gmail.com>
2025-05-09 17:56:44 +0200

    feat: add matchmaking algorithm

--- src/server/src/services/matchmaking/opponentSelectionLogic.ts
+++ src/server/src/services/matchmaking/opponentSelectionLogic.ts
@@ -1,16 +1,13 @@
 import { readPlayerByUsername } from '../../repositories/player';
 
-export const isValidMatch = async (candidate1: string, candidate2: string): Promise<boolean> => {
-  // TODO : UNCOMMENT THIS CODE WHEN PLAYERS ARE SAVED IN DB
-
+export const isValidMatch = async (candidate1: string, candidate2: string, maxDiff: number): Promise<boolean> => {
   // retrieve candidates' ratings
-  // const player1 = await readPlayerByUsername(candidate1);
-  // const player2 = await readPlayerByUsername(candidate2);
+  const player1 = await readPlayerByUsername(candidate1);
+  const player2 = await readPlayerByUsername(candidate2);
 
-  // if (!player1 || !player2) {
-  //   return false;
-  // }
+  if (!player1 || !player2) {
+    return false;
+  }
 
-  // TODO : implement the logic to evaluate the match
-  return true;
+  return Math.abs(Number(player1.rating) - Number(player2.rating)) <= maxDiff;
 };
