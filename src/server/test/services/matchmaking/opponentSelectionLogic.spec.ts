a8db840dcea92344f135cdec3b7447502c262d0d
Oldranda1414 <leonardo.randacio@gmail.com>
2025-05-09 16:58:49 +0200

    refactor: improved is valid match matchmaking algorithm

--- src/server/test/services/matchmaking/opponentSelectionLogic.spec.ts
+++ src/server/test/services/matchmaking/opponentSelectionLogic.spec.ts
@@ -1,13 +1,14 @@
 import { PlayerFactory } from '../../../src/models/Player';
 import { RatingFactory } from '../../../src/models/Rating';
-import { evaluateOpponentMatch } from '../../../src/services/matchmaking/opponentSelectionLogic';
+import { isValidMatch } from '../../../src/services/matchmaking/opponentSelectionLogic';
 import { jest, describe, it, expect } from '@jest/globals';
 
 jest.mock('../../../src/repositories/player', () => ({
   readPlayerByUsername: jest.fn((username: string) => {
-    if(username === 'validUsername') return PlayerFactory.create(username, RatingFactory.create(1500, 0, 0));
+    if (username === 'validUsername')
+      return PlayerFactory.create(username, RatingFactory.create(1500, 0, 0));
     return null;
-  })
+  }),
 }));
 
 describe('opponentSelectionLogic', () => {
@@ -15,7 +16,6 @@ describe('opponentSelectionLogic', () => {
   //   it('should return false if either player does not exist', async () => {
   //     const validUsername: string = 'validUsername';
   //     const nonExistingUsername: string = 'nonExistingUsername'
-
   //     let result = await evaluateOpponentMatch(validUsername, new Date(), nonExistingUsername, new Date());
   //     expect(result).toEqual(false);
   //     result = await evaluateOpponentMatch(nonExistingUsername, new Date(), validUsername, new Date());
