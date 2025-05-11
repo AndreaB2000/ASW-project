4482f1fd385273a68dae8ae5660d0eb8c9d438aa
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-05-11 17:55:09 +0200

    test(middleware): test authentication socket middleware

--- src/server/test/middlewares/auth.spec.ts
+++ src/server/test/middlewares/auth.spec.ts
@@ -2,8 +2,8 @@ import express, { Response } from 'express';
 import request from 'supertest';
 import cookieParser from 'cookie-parser';
 import jwt from 'jsonwebtoken';
-import { describe, it, expect, beforeEach } from '@jest/globals';
-import { authenticateToken } from '../../src/middlewares/auth';
+import { describe, it, expect, beforeEach, jest } from '@jest/globals';
+import { authenticateToken, authenticateTokenSocket } from '../../src/middlewares/auth';
 import { expiration, secret } from '../../src/config/jwt';
 
 describe('authenticateToken middleware using cookies', () => {
@@ -20,8 +20,14 @@ describe('authenticateToken middleware using cookies', () => {
       });
     });
 
-    it('should return 401 if no token is provided', async () => {
+    it('should return 401 if no cookies are sent', async () => {
       const response = await request(app).get('/protected');
+      expect(response.status).toBe(401);
+      expect(response.body.message).toBe('Unauthorized');
+    });
+
+    it('should return 401 if no token is provided', async () => {
+      const response = await request(app).get('/protected').set('Cookie', 'token=');
       expect(response.status).toBe(401);
       expect(response.body.message).toBe('Unauthorized');
     });
@@ -63,8 +69,67 @@ describe('authenticateToken middleware using cookies', () => {
   });
 
   describe('socket.io', () => {
-    it('succeed', () => {
-      expect(true).toBe(true);
+
+    const mockNext = jest.fn();
+
+    beforeEach(() => {
+      jest.clearAllMocks();
     });
+
+    it('should call next with error if no cookies are sent', () => {
+      const socketWithoutCookies = {
+        handshake: {
+          headers: {},
+        },
+      };
+      authenticateTokenSocket(socketWithoutCookies as any, mockNext);
+      expect(mockNext).toHaveBeenCalledWith(new Error('No cookies sent'));
+    });
+
+    it('should call next with error if no token is found in cookies', () => {
+      const socketWithoutToken = {
+        handshake: {
+          headers: {
+            cookie: 'c1=val1; c2=val2',
+          },
+        },
+      };
+      authenticateTokenSocket(socketWithoutToken as any, mockNext);
+      expect(mockNext).toHaveBeenCalledWith(new Error('No token found in cookies'));
+    });
+
+    it('should call next with error if token is invalid', () => {
+      const socketWithInvalidToken = {
+        handshake: {
+          headers: {
+            cookie: 'token=invalid'
+          },
+        },
+      };
+      authenticateTokenSocket(socketWithInvalidToken as any, mockNext);
+      expect(mockNext).toHaveBeenCalledWith(new Error('Invalid token'));
+    });
+
+    it('should call next() and set account in socket if token is valid', () => {
+      const testAccount = {
+        username: 'testuser',
+        email: 'test@user.com',
+      };
+      const validToken = jwt.sign(testAccount, 'test_secret', { expiresIn: '1h' });
+      jest.spyOn(jwt, 'verify').mockReturnValue(testAccount as any);
+      const mockNext = jest.fn();
+      const socketWithValidToken = {
+        handshake: {
+          headers: {
+            cookie: `token=${validToken}`,
+          },
+          auth: {},
+        },
+      };
+      authenticateTokenSocket(socketWithValidToken as any, mockNext);
+      expect(mockNext).toHaveBeenCalledWith(); // called with no error
+      expect((socketWithValidToken.handshake.auth as any).account).toEqual(testAccount);
+    });
+
   });
 });
