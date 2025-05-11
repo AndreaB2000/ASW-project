8247bad042ae23cb4dc5cf9105e0233463cad6dd
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-05-11 13:22:06 +0200

    feat(middleware): correct auth middleware

--- src/server/test/middlewares/auth.spec.ts
+++ src/server/test/middlewares/auth.spec.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import request from 'supertest';
+import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken';
 import { describe, it, expect, beforeEach } from '@jest/globals';
 import { authenticateToken } from '../../src/middlewares/auth';
+import { expiration, secret } from '../../src/config/jwt';
 
 describe('authenticateToken middleware using cookies', () => {
   let app: express.Express;
@@ -9,14 +12,50 @@ describe('authenticateToken middleware using cookies', () => {
   beforeEach(() => {
     app = express();
     app.use(express.json());
-    app.get('/protected', authenticateToken, (_req: Request, res: Response) => {
-      res.status(200).json({ message: 'Access granted' });
+    app.use(cookieParser());
+    app.get('/protected', authenticateToken, (req: Request, res: Response) => {
+      res.status(200).json({ message: 'Access granted', account: req.body.account });
     });
   });
 
-  it('returns always', async () => {
-    const res = await request(app).get('/protected');
-    expect(res.status).toBe(200);
-    expect(res.body).toEqual({ message: 'Access granted' });
+  it('should return 401 if no token is provided', async () => {
+    const response = await request(app).get('/protected');
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Unauthorized');
+  });
+
+  it('should return 401 if an invalid token is provided', async () => {
+    const response = await request(app)
+      .get('/protected')
+      .set('Cookie', 'token=invalidtoken');
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Invalid token');
+  });
+
+  it('should call next() if a valid token is provided', async () => {
+    const validToken = jwt.sign(
+      {},
+      secret,
+      { expiresIn: expiration },
+    );
+    const response = await request(app)
+      .get('/protected')
+      .set('Cookie', `token=${validToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Access granted');
+  });
+
+  it('should insert account data in body if a valid token is provided', async () => {
+    const testAccount = {
+      username: 'testuser',
+      email: 'test@user.com'
+    };
+    const validToken = jwt.sign(testAccount, secret, { expiresIn: expiration });
+    const response = await request(app)
+      .get('/protected')
+      .set('Cookie', `token=${validToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.account.username).toBe(testAccount.username);
+    expect(response.body.account.email).toBe(testAccount.email);
   });
 });
