d8989a863c74b1bd3d7126f62c3ee1375bf77fe1
Oldranda1414 <leonardo.randacio@gmail.com>
2025-05-11 16:50:08 +0200

    feat: add rating to matchmaking candidate

--- src/server/test/models/matchmakingCandidate.spec.ts
+++ src/server/test/models/matchmakingCandidate.spec.ts
@@ -3,19 +3,24 @@ import {
   MatchmakingCandidateFactory,
 } from '../../src/models/MatchmakingCandidate';
 import { describe, it, expect, jest } from '@jest/globals';
+import { Rating, RatingFactory } from '../../src/models/Rating';
 
 const playerId: string = 'testPlayerId';
+const playerRatingValue: number = 1500;
+const playerRating: Rating = RatingFactory.create(playerRatingValue);
 const requestTime: Date = new Date('2023-10-01T00:00:00Z');
 
 describe('matchmaking candidate factory', () => {
   it('should create a correct new matchmaking candidate', async () => {
     const candidate: MatchmakingCandidate = MatchmakingCandidateFactory.create(
       playerId,
+      playerRating,
       requestTime,
     );
 
     expect(candidate).not.toBeNull();
     expect(candidate.username).toBe(playerId);
+    expect(candidate.rating).toEqual(playerRating);
     expect(candidate.requestTime).toEqual(requestTime);
   });
 
@@ -24,10 +29,11 @@ describe('matchmaking candidate factory', () => {
     const mockNow = new Date('2024-10-01T00:00:00Z');
     global.Date = jest.fn(() => mockNow) as any;
 
-    const candidate: MatchmakingCandidate = MatchmakingCandidateFactory.create(playerId);
+    const candidate: MatchmakingCandidate = MatchmakingCandidateFactory.create(playerId, playerRating);
 
     expect(candidate).not.toBeNull();
     expect(candidate.username).toBe(playerId);
+    expect(candidate.rating).toEqual(playerRating);
     expect(candidate.requestTime).toEqual(mockNow);
   });
 });
