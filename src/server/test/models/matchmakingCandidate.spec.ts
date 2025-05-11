b656b1f1ec0bcdd964ad5c64e4a094db4627f733
Oldranda1414 <leonardo.randacio@gmail.com>
2025-05-11 16:12:24 +0200

    feat: add default constructor to matchmakingCandidate

--- src/server/test/models/matchmakingCandidate.spec.ts
+++ src/server/test/models/matchmakingCandidate.spec.ts
@@ -2,7 +2,7 @@ import {
   MatchmakingCandidate,
   MatchmakingCandidateFactory,
 } from '../../src/models/MatchmakingCandidate';
-import { describe, it, expect } from '@jest/globals';
+import { describe, it, expect, jest } from '@jest/globals';
 
 const playerId: string = 'testPlayerId';
 const requestTime: Date = new Date('2023-10-01T00:00:00Z');
@@ -17,5 +17,17 @@ describe('matchmaking candidate factory', () => {
     expect(candidate).not.toBeNull();
     expect(candidate.username).toBe(playerId);
     expect(candidate.requestTime).toEqual(requestTime);
+  });
+
+  it('should create a candidate with request time now if no time is passed', async () => {
+    // Mock the Date constructor
+    const mockNow = new Date('2023-10-01T00:00:00Z');
+    global.Date = jest.fn(() => mockNow) as any;
+
+    const candidate: MatchmakingCandidate = MatchmakingCandidateFactory.create(playerId);
+
+    expect(candidate).not.toBeNull();
+    expect(candidate.username).toBe(playerId);
+    expect(candidate.requestTime).toEqual(mockNow);
   });
 });
