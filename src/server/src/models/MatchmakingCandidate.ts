471cf462c65448873723fa6ddbc44fb530ba65c4
Oldranda1414 <leonardo.randacio@gmail.com>
2025-05-13 16:22:07 +0200

    feat: add equals to matchmaking candidate

--- src/server/src/models/MatchmakingCandidate.ts
+++ src/server/src/models/MatchmakingCandidate.ts
@@ -1,9 +1,27 @@
 import { Rating } from "./Rating";
 
 export interface MatchmakingCandidate {
+  /**
+   * The username of the candidate.
+   */
   get username(): string;
+
+  /**
+   * The rating of the candidate.
+   */
   get rating(): Rating;
+
+  /**
+   * The time when the candidate made the matchmaking request.
+   */
   get requestTime(): Date;
+
+  /**
+   * Compares this candidate with another candidate.
+   * @param other the other candidate to compare with.
+   * @return true if the candidates are equal, false otherwise.
+   */
+  equals(other: MatchmakingCandidate): boolean;
 }
 
 export class MatchmakingCandidateFactory {
@@ -32,5 +50,10 @@ class MatchmakingCandidateImpl implements MatchmakingCandidate {
 
   get requestTime(): Date {
     return this._requestTime;
+  }
+
+  //  TODO UPDATE THIS WHEN THE USERS WILL NOT BE IDENTIFIED BY USERNAME
+  equals(other: MatchmakingCandidate): boolean {
+    return this._username === other.username;
   }
 }
