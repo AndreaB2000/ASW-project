6c74a0d1b81497e58118d14ce29b510cc3197531
Oldranda1414 <leonardo.randacio@gmail.com>
2025-05-13 17:51:10 +0200

    refactor: improve matchmaking controller structure

--- src/server/src/controllers/matchmaking.ts
+++ src/server/src/controllers/matchmaking.ts
@@ -1,32 +1,40 @@
 import { findMatchOrQueue } from '../services/matchmaking/matchmaking';
 import { emitUsername } from '../routes/root'
-import { newMatch } from '../services/match';
 import { getPlayerSocket, registerPlayerSocket } from '../sockets/socket';
 import { Socket } from 'socket.io';
-import * as ioHandler from '../sockets/socket';
+import { getIO } from '../sockets/socket';
 
 export const requestMatch = async (playerSocket: Socket, username: string): Promise<string | undefined> => {
-    // If this is out of the function, io is null TODO REMOVE THIS SHOULD NOW BE NECESSARY
-    const io = ioHandler.getIO();
 
     registerPlayerSocket(username, playerSocket);
 
-    const opponentUsername = await findMatchOrQueue(username);
-    if (opponentUsername) {
-        const matchId = await newMatch(username, opponentUsername, new Date());
+    const result = await findMatchOrQueue(username);
 
-        // add players to room with matchId
-        playerSocket.join(matchId);
-        getPlayerSocket(opponentUsername).join(matchId);
-
-        // sending matchId to players
-        emitUsername(username, 'matchFound', matchId);
-        emitUsername(opponentUsername, 'matchFound', matchId);
-
-        // this will not be necessary TODO REMOVE
-        io.to(matchId).emit('matchStart', matchId);
-
-        return matchId;
+    if (!result) {
+        return undefined;
     }
-    return undefined;
+
+    const [ usernameA, usernameB, matchId ] = result;
+    notifyNewMatch(usernameA, usernameB, matchId);
+
+    return matchId;
+};
+
+// TODO: MOVE THIS TO MATCH CONTROLLER
+export const notifyNewMatch = async (usernameA: string, usernameB: string, matchId: string): Promise<void> => {
+    [usernameA, usernameB].forEach((username) => {
+        const socket = getPlayerSocket(username);
+        if (!socket) {
+            console.error(`Socket not found for user: ${username}`);
+            return;
+        }
+        socket.join(matchId);
+        emitUsername(username, 'matchFound', matchId);
+    });
+
+    // If this is out of the function, io is null TODO REMOVE THIS SHOULD NOT BE NECESSARY
+    const io = getIO();
+
+    // this will not be necessary TODO REMOVE
+    io.to(matchId).emit('matchStart', matchId);
 };
