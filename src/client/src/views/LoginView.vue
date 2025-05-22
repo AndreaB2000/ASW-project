09ddc5df58cd9ac37351a646b0a9d8864ee072ff
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-05-22 18:03:58 +0200

    feat(profile): create path in the client to arrive to profile page

--- src/client/src/views/LoginView.vue
+++ src/client/src/views/LoginView.vue
@@ -16,7 +16,7 @@ function login(event: Event) {
   server.post('/account/login', { username: form.username, password: form.password })
     .then(response => {
       console.log('Login successful:', response.data);
-      router.push('/')
+      router.push('/play');
     })
     .catch(error => {
       console.error('Login error:', error);
--- src/client/src/views/PlayView.vue
+++ src/client/src/views/PlayView.vue
@@ -2,4 +2,8 @@
 
 <template>
   <p style="font-size: large">you are now logged in</p>
+  <button>Play</button>
+  <button>Tutorial</button>
+  <button>Leaderboard</button>
+  <button @click="$router.push('/profile')">Profile</button>
 </template>
