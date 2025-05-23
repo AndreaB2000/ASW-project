41207d865a483f996cca7ea3af699616afda8934
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-05-23 10:06:50 +0200

    feat(profile): change from editing username to editing email in client

--- src/client/src/views/ProfileView.vue
+++ src/client/src/views/ProfileView.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { useUserStore } from '@/stores/userStore'
+import { socket } from '@/services/server-connections'
 
 const userStore = useUserStore();
 
-const username = ref(userStore.username);
+const email = ref(userStore.email);
 const isEditing = ref(false);
 
 function changeProfile() {
@@ -12,7 +13,10 @@ function changeProfile() {
 }
 
 function saveProfile() {
-  isEditing.value = false;
+  socket.emit('updateEmail', email.value, () => {
+    userStore.setEmail(email.value);
+    isEditing.value = false;
+  });
 }
 </script>
 
@@ -20,13 +24,13 @@ function saveProfile() {
   <p style="font-size: large">profile</p>
   <section>
     <img alt="profile image"/>
-
+    <h1>{{ userStore.username }}</h1>
     <div v-if="!isEditing">
-      <h1>{{ username }}</h1>
+      <h1>{{ email }}</h1>
       <button @click="changeProfile()">Modify</button>
     </div>
     <div v-else>
-      <input v-model="username" />
+      <input v-model="email" />
       <button @click="saveProfile()">Save</button>
     </div>
   </section>
