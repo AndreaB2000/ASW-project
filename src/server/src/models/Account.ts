3fd267df5c82ad05ff7a3f654dd7769bd61ba88f
Oldranda1414 <leonardo.randacio@gmail.com>
2025-05-23 10:49:17 +0200

    refactor: merged player concept into account concept

--- src/server/src/models/Account.ts
+++ src/server/src/models/Account.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import { Rating, RatingFactory } from './Rating';
 
 /**
  * Value object representing an account.
@@ -17,64 +18,74 @@ export interface Account {
    */
   get hashedPassword(): string;
   /**
+   * Rating of the account
+   */
+  get rating(): Rating;
+  /**
    * Checks if the given password matches the hashed password.
    * @param password the password to check
    */
   checkPassword(password: string): Promise<boolean>;
 }
 
-export class AccountFactory {
-  /**
-   * Creates a new account with the given username and password. The password will be hashed.
-   * @param username the username of the user
-   * @param email the email of the user
-   * @param password the password of the user
-   * @returns {Account} the account object
-   */
-  static async createWithHashing(
-    username: string,
-    email: string,
-    password: string,
-  ): Promise<Account> {
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
-    return new AccountImpl(username, email, hashedPassword);
-  }
+/**
+ * Account factory. Creates a new account with the given username and password. The password will be hashed.
+ * @param username the username of the user
+ * @param email the email of the user
+ * @param password the password of the user
+ * @param rating the rating of the user
+ * @returns {Account} the account object
+ */
+export const createWithHashing = async (
+  username: string,
+  email: string,
+  password: string,
+  rating?: Rating,
+): Promise<Account> => {
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return new AccountImpl(username, email, hashedPassword, rating);
+};
 
-  /**
-   * Creates a new account with the given username and hashed password.
-   * @param username the username of the user
-   * @param email the email of the user
-   * @param hashedPassword the hashed password of the user
-   * @returns {Account} the account object
-   */
-  static create(username: string, email: string, hashedPassword: string): Account {
-    return new AccountImpl(username, email, hashedPassword);
-  }
-}
+/**
+ * Account factory. Creates a new account with the given username and hashed password.
+ * @param username the username of the user
+ * @param email the email of the user
+ * @param hashedPassword the hashed password of the user
+ * @param rating the rating of the user
+ * @returns {Account} the account object
+ */
+export const create = (username: string, email: string, hashedPassword: string, rating?: Rating): Account =>
+  new AccountImpl(username, email, hashedPassword, rating);
 
 class AccountImpl implements Account {
-  private name: string;
-  private mail: string;
-  private psw: string;
+  private _username: string;
+  private _email: string;
+  private _hashedPassword: string;
+  private _rating: Rating;
 
   get username(): string {
-    return this.name;
+    return this._username;
   }
   get email(): string {
-    return this.mail;
+    return this._email;
   }
   get hashedPassword(): string {
-    return this.psw;
+    return this._hashedPassword;
   }
 
-  constructor(username: string, email: string, password: string) {
-    this.name = username;
-    this.mail = email;
-    this.psw = password;
+  get rating(): Rating {
+    return this._rating;
+  }
+
+  constructor(username: string, email: string, password: string, rating?: Rating) {
+    this._username = username;
+    this._email = email;
+    this._hashedPassword = password;
+    this._rating = rating ?? RatingFactory.create();
   }
 
   async checkPassword(password: string): Promise<boolean> {
-    return await bcrypt.compare(password, this.psw);
+    return await bcrypt.compare(password, this._hashedPassword);
   }
 }
