4658771e8a0392cc67d5134f1730fb995f63cf23
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-05-23 10:06:50 +0200

    feat(profile): change from editing username to editing email in client

--- src/client/src/stores/userStore.ts
+++ src/client/src/stores/userStore.ts
@@ -1,12 +1,17 @@
 import { defineStore } from 'pinia';
 
+/**
+ * User store
+ * @description This store is used to manage user-related data and actions.
+ */
 export const useUserStore = defineStore('user', {
   state: () => ({
     username: '',
+    email: ''
   }),
   actions: {
-    setUsername(name: string) {
-      this.username = name;
+    setEmail(email: string) {
+      this.email = email;
     },
   },
 });
