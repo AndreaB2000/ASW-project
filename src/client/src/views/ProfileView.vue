0e986973db70ebac068494785ed5de371dbda907
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-05-26 17:57:39 +0200

    feat(WIP): work at updating the email

--- src/client/src/views/ProfileView.vue
+++ src/client/src/views/ProfileView.vue
@@ -1,11 +1,22 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { useUserStore } from '@/stores/userStore'
-import { socket } from '@/services/server-connections'
+import { socket, server } from '@/services/server-connections'
 
 const userStore = useUserStore();
 
-const email = ref(userStore.email);
+if (userStore.username === '' || userStore.email === '') {
+  server.get('/account/me').then(response => {
+    userStore.setUsername(response.data.username);
+    userStore.setEmail(response.data.email);
+  }).catch((error: Error) => {
+    console.error('Failed to fetch user data:', error);
+    // Optionally redirect to login or show an error message
+  });
+}
+
+const reactiveEmail = ref(userStore.email);
+
 const isEditing = ref(false);
 
 function changeProfile() {
@@ -13,8 +24,10 @@ function changeProfile() {
 }
 
 function saveProfile() {
-  socket.emit('changeEmail', email.value, () => {
-    userStore.setEmail(email.value);
+  console.log('Saving profile with email:', reactiveEmail.value);
+  socket.emit('change email', reactiveEmail.value, () => {
+    console.log('Email change request sent');
+    userStore.setEmail(reactiveEmail.value);
     isEditing.value = false;
   });
 }
@@ -24,13 +37,12 @@ function saveProfile() {
   <p style="font-size: large">profile</p>
   <section>
     <img alt="profile image"/>
-    <h1>{{ userStore.username }}</h1>
+    <h1>{{ userStore.email }}</h1>
     <div v-if="!isEditing">
-      <h1>{{ email }}</h1>
       <button @click="changeProfile()">Modify</button>
     </div>
     <div v-else>
-      <input v-model="email" />
+      <input v-model="reactiveEmail" />
       <button @click="saveProfile()">Save</button>
     </div>
   </section>
