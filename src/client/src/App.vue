23d4bdbef524a422c7ad16b7742674ad4c53c8f3
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-05-29 16:27:03 +0200

    feat(theme): improve theme management in front end

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import 'mdb-vue-ui-kit/css/mdb.min.css';
+import './styles/themes.scss';
+import { useThemeStore } from './stores/themeStore';
+
+const themeStore = useThemeStore();
+console.log('Current theme:', themeStore.currentTheme);
+themeStore.setTheme(themeStore.currentTheme);
 </script>
 
 <template>
