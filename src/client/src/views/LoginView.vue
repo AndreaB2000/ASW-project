201ea1b93e1a040c900c2b1f24becb5ca70261da
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-06-04 13:10:13 +0200

    feat: rework login so it uses AuthScreen

--- src/client/src/views/LoginView.vue
+++ src/client/src/views/LoginView.vue
@@ -3,9 +3,10 @@ import { server, tryAuth } from '@/services/server-connections';
 import { reactive } from 'vue';
 import { useRouter } from 'vue-router';
 import { useUserStore } from '@/stores/userStore';
-import { MDBBtn, MDBCol, MDBContainer, MDBInput, MDBRow } from 'mdb-vue-ui-kit';
+import { MDBBtn, MDBInput } from 'mdb-vue-ui-kit';
 import DialogModal from '@/components/DialogModal.vue';
 import { isPasswordStrong, isUsernameStrong } from '@/services/strength-validator';
+import AuthScreen from '@/components/AuthScreen.vue';
 
 const router = useRouter();
 const userStore = useUserStore();
@@ -25,7 +26,7 @@ const dialog = reactive({
   text: ''
 });
 
-const changeSocketNamespace = (namespace: string) => {
+function changeSocketNamespace(namespace: string) {
   // This function should change the socket namespace, implementation depends on your socket setup
   console.log(`Changing socket namespace to: ${namespace}`);
   tryAuth();
@@ -33,7 +34,9 @@ const changeSocketNamespace = (namespace: string) => {
 
 function login(event: Event) {
   event.preventDefault();
-  if (!isPasswordStrong(form.password) || !isUsernameStrong(form.username)) return;
+  validation.username = isUsernameStrong(form.username);
+  validation.password = isPasswordStrong(form.password);
+  if (!validation.username || !validation.password) return;
   server
     .post('/account/login', { username: form.username, password: form.password })
     .then(response => {
@@ -52,54 +55,39 @@ function login(event: Event) {
 </script>
 
 <template>
-  <img
-    src="../assets/landingIcon.svg"
-    alt="Landing Icon"
-    class="position-absolute top-0 start-0 m-3"
-    style="height: 40px;
-    z-index: 100;
-    cursor: pointer;"
-    @click="$router.push('/')"
-  />
-  <MDBContainer class="vh-100 d-flex justify-content-center align-items-center">
-    <MDBRow class="w-100">
-      <MDBCol class="md-6 d-none d-md-flex justify-content-center align-items-center">
-        <img src="../assets/sandpiles-template-img.svg" alt="Sandpiles Template" class="img-fluid" style="max-height: 60vh;" />
-      </MDBCol>
-      <MDBCol class="md-6 d-flex justify-content-center align-items-center">
-        <div class="w-100" style="max-width: 400px;">
-          <h1 class="text-center mb-4 fw-bold">LOGIN</h1>
-          <MDBRow tag="form" class="g-3 needs-validation" novalidate @submit.prevent="login">
-            <MDBInput
-              type="text"
-              label="Username"
-              id="username"
-              v-model="form.username"
-              :class="{
-                'is-invalid': validation.username === false,
-                'is-valid': validation.username === true
-              }"
-              white
-            />
-            <MDBInput
-              type="password"
-              label="Password"
-              id="password"
-              v-model="form.password"
-              wrapperClass="mb-4"
-              :class="{
-                'is-invalid': validation.password === false,
-                'is-valid': validation.password === true
-              }"
-              white
-            />
-            <MDBBtn color="primary" block type="submit"> Login </MDBBtn>
-          </MDBRow>
-          <p class="text-center mt-3">Don't have an account yet?</p>
-          <MDBBtn color="secondary" block @click="$router.push('/registration')">Register</MDBBtn>
-        </div>
-      </MDBCol>
-    </MDBRow>
-  </MDBContainer>
+  <AuthScreen title="Login" :formCompletionCallback="login">
+    <template #form-fields>
+      <MDBInput
+        type="text"
+        label="Username"
+        id="username"
+        v-model="form.username"
+        :class="{
+          'is-invalid': validation.username === false,
+          'is-valid': validation.username === true
+        }"
+        wrapperClass="mb-4"
+        invalidFeedback="Username must be at least 3 characters long and contain only alphanumeric characters."
+        white
+      />
+      <MDBInput
+        type="password"
+        label="Password"
+        id="password"
+        v-model="form.password"
+        wrapperClass="mb-4"
+        :class="{
+          'is-invalid': validation.password === false,
+          'is-valid': validation.password === true
+        }"
+        invalidFeedback="Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+        white
+      />
+    </template>
+    <template #secondary-action>
+      <p class="text-center mt-3">Don't have an account yet?</p>
+      <MDBBtn color="secondary" block @click="$router.push('/registration')">Register</MDBBtn>
+    </template>
+  </AuthScreen>
   <DialogModal v-model="dialog.visible" :text=dialog.text @close="dialog.visible = false" />
 </template>
