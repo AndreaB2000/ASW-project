ef1b0f6f5eb0ea6766219dcc0b4910f3754ad1b4
Oldranda1414 <leonardo.randacio@gmail.com>
2025-06-05 16:04:55 +0200

    feat: add functionality to play button in play page

--- src/client/src/views/PlayView.vue
+++ src/client/src/views/PlayView.vue
@@ -1,8 +1,23 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { socket } from '@/services/server-connections';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function playPVP() {
+  console.log('enterQueue');
+  socket.emit('requestMatch');
+}
+
+socket.on('matchFound', (matchId: string) => {
+  router.push({ path: '/match', query: { id: matchId } });
+});
+
+</script>
 
 <template>
   <p style="font-size: large">you are now logged in</p>
-  <button>Play</button>
+  <button @click="playPVP">Play</button>
   <button>Tutorial</button>
   <button>Leaderboard</button>
   <button @click="$router.push('/profile')">Profile</button>
