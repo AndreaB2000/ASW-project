a5341f4bac3dbdab623d45423c8bbed653be12d7
Oldranda1414 <leonardo.randacio@gmail.com>
2025-06-10 17:38:22 +0200

    feat: implement client side leaderboard

--- src/server/src/routes/auth.ts
+++ src/server/src/routes/auth.ts
@@ -4,6 +4,7 @@ import { match } from './match';
 import { authMatchmaking } from './matchmaking';
 import { account } from './account';
 import { disconnect } from '../sockets/disconnect';
+import { leaderboard } from './leaderboard';
 
 export const auth = async (socket: Socket) => {
   console.log('User connected to auth');
@@ -15,6 +16,7 @@ export const auth = async (socket: Socket) => {
   match(socket);
   authMatchmaking(socket);
   account(socket);
+  leaderboard(socket);
 };
 
 export const emitToRoom = (room: string, event: string, ...data: any[]) => {
