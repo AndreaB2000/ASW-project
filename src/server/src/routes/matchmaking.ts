5f135d7c44eca963cd34d2dcd1c180324efb3551
Oldranda1414 <leonardo.randacio@gmail.com>
2025-06-15 16:03:11 +0200

    feat: improve guest matchmaking

--- src/server/src/routes/matchmaking.ts
+++ src/server/src/routes/matchmaking.ts
@@ -1,5 +1,6 @@
 import { Socket } from 'socket.io';
 import { requestMatch, requestMatchWithBot } from '../controllers/matchmaking';
+import { getPlayerSockets } from '../sockets/socket';
 
 export const authMatchmaking = (socket: Socket) => {
   socket.on('requestMatch', async data => {
@@ -13,12 +14,36 @@ export const authMatchmaking = (socket: Socket) => {
 
 export const guestMatchmaking = (socket: Socket) => {
   socket.on('requestMatch', async data => {
+    let guestUsername: string = socket.data.guestUsername;
+    if (!guestUsername) {
+      guestUsername = setGuestUsername(socket);
+    }
     await requestMatch(socket, guestUsername);
   });
 
   socket.on('requestMatchWithBot', async data => {
+    let guestUsername: string = socket.data.guestUsername;
+    if (!guestUsername) {
+      guestUsername = setGuestUsername(socket);
+    }
     await requestMatchWithBot(socket, guestUsername);
   });
+};
 
-  const guestUsername = `guest`;
+const setGuestUsername = (socket: Socket): string => {
+  let guestUsername: string;
+  if (!socket.data.guestUsername) {
+    guestUsername = getGuestUsername(socket, 0);
+    socket.data.username = guestUsername;
+  }
+  return guestUsername;
+};
+
+const getGuestUsername = (socket: Socket, postfix: number) => {
+  const candidateUsername = `guest-${socket.id}`;
+  if (getPlayerSockets().has(candidateUsername)) {
+    return candidateUsername;
+  } else {
+    return getGuestUsername(socket, postfix + 1);
+  }
 };
