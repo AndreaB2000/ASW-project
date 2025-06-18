277e7083dcb2d37c3619962599b8fadff5f69fff
Oldranda1414 <leonardo.randacio@gmail.com>
2025-06-18 15:27:17 +0200

    feat: implement rating api and rating service client side

---
+++ src/client/src/services/rating.ts
@@ -0,0 +1,61 @@
+import { roundToDecimal } from '../utils/round';
+
+/**
+ * enum GameResult
+ * This enum represents the possible results of a game.
+ * - WinA: Player A wins
+ * - WinB: Player B wins
+ * @enum {string}
+ * @property {string} WinA - Player A wins
+ * @property {string} WinB - Player B wins
+ */
+export enum GameResult {
+  WinA = 'WinA',
+  WinB = 'WinB',
+}
+
+/**
+ * Calculate the rating change for a player based on the game result.
+ * @param ratingA - The current rating of Player A.
+ * @param ratingB - The current rating of Player B.
+ * @param result - The result of the game.
+ * @param kFactor - The K-factor used in the Elo rating system. Default is 32.
+ * @returns The rating change for Player A, rounded to 1 decimal place.
+ */
+export const getRatingChange = (
+  ratingA: number,
+  ratingB: number,
+  result: GameResult,
+  kFactor: number,
+): number => {
+  const expectedScoreA = getExpectedScore(ratingA, ratingB);
+  let scoreA: number;
+  let scoreB: number;
+  switch (result) {
+    case GameResult.WinA:
+      scoreA = 1;
+      scoreB = 0;
+      break;
+    case GameResult.WinB:
+      scoreA = 0;
+      scoreB = 1;
+      break;
+  }
+  const ratingUpdateA = kFactor * (scoreA - expectedScoreA);
+
+  const roundedRatingUpdate = roundToDecimal(ratingUpdateA, 1);
+
+  return roundedRatingUpdate;
+};
+
+/**
+ * Calculate the expected score for Player A based on two player ratings.
+ * The expected score is a value between 0 and 1, representing the probability of Player A winning.
+ * @param ratingA - The current rating of Player A.
+ * @param ratingB - The current rating of Player B.
+ * @returns The expected score for Player A.
+ */
+const getExpectedScore = (ratingA: number, ratingB: number): number => {
+  const expectedScoreA = 1 / (1 + Math.pow(10, (ratingB - ratingA) / 400));
+  return expectedScoreA;
+};
